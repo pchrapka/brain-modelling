c0d20b6b6c7518fb9178473b56aa6f6378e6d14a
pchrapka <pchrapka@gmail.com>
2016-02-02 10:10:18 -0500

    formatted the code better

--- methods/QRDLSL.m
+++ methods/QRDLSL.m
@@ -3,85 +3,112 @@ classdef QRDLSL < handle
     %   Detailed explanation goes here
     
     properties
+        % filter variables
+        Berrord;
+        Bpowerdd;
+        Fpowerd;
+        pbd;
+        pfd;
+        
+        % filter order
+        M;
     end
     
     methods
-        function obj = QRDLSL(obj, order,)
+        function obj = QRDLSL(order)
+            
+            obj.M = order;
+            
+            delta = 0.1; % small positive constant
+            obj.Bpowerdd = delta*ones(obj.M+1,1);
+            obj.Fpowerd = obj.Bpowerdd;
+            
+            zeroVec = zeros(obj.M,1);
+            obj.pbd = zeroVec;
+            obj.pfd = zeroVec;
         end
         
         function obj = update(obj, x)
             %   x
             %       new measurement
             
-            Bpower_new = zeros(M,1);
-            Fpower_new = zeros(M,1);
-            % missing Ferror for current iteration
+            % Mem allocation
+            zeroVec = zeros(obj.M,1);
+            Berror = zeroVec;
+            Ferror = zeroVec;
+            gammad = zeroVec;
             
-            % Extract params from previous update
-            % Berror,
-            % gamma?
-            % Berror comes from previous udpate
-            for m=1:M
+            pf = zeroVec;
+            pb = zeroVec;
+            Bpowerd = zeroVec;
+            Fpower = zeroVec;
+            
+            % Data initialization
+            Berror(1) = x;
+            Ferror(1) = x;
+            gammad(1) = 1; % i'm assuming this is right
+            
+            for m=2:M+1
                 
                 % Adaptive Forward prediction
                 % $XB * \PhiB = ZB$
                 
-                % TODO Missing vars
-                % Ferror(m-1)
-                % gamma(m-1)
-                
                 % Set up vars
                 XB = zeros(2,3);
-                XB(1,1) = sqrt(lambda*Bpower(m-1));
-                XB(1,2) = Berror(m-1);
-                XB(2,1) = sqrt(lambda)*conj(pf(m-1));
+                XB(1,1) = sqrt(lambda*obj.Berrordd(m-1));
+                XB(1,2) = obj.Berrord(m-1);
+                XB(2,1) = sqrt(lambda)*conj(obj.pfd(m-1));
                 XB(2,2) = Ferror(m-1);
-                XB(3,2) = sqrt(gamma(m-1));
+                XB(3,2) = sqrt(gammad(m-1));
                 
-                Bpower_new(m-1) = lambda*Bpower(m-1) * Berror(m-1)*conj(Berror(m-1));
+                Bpowerd(m-1) = lambda*obj.Berrordd(m-1) ...
+                    + obj.Berrord(m-1)*conj(obj.Berrord(m-1));
                 
-                cb = sqrt(lambda*Bpower(m-1))/sqrt(Bpower_new(m-1));
-                sb = Berror(m-1)/sqrt(Bpower_new(m-1));
+                cb = sqrt(lambda*obj.Berrordd(m-1))/sqrt(Bpowerd(m-1));
+                sb = obj.Berrord(m-1)/sqrt(Bpowerd(m-1));
                 PhiB = [ cb -sb; conj(sb) cb];
                 
                 % Compute
                 ZB = XB*PhiB;
                 
                 % Extract vars
-                pf_new(m-1) = conj(ZB(2,1)); % overwrite pf?
+                pf(m-1) = conj(ZB(2,1)); % overwrite pf?
                 Ferror(m) = ZB(2,2);
-                gamma(m) = ZB(3,2)^2;
-                Bpower = Bpower_new; % do whole vector outside of loop?
+                gammad(m) = ZB(3,2)^2;
                 
                 % Forward prediction
                 % $ XF * \PhiF = ZF$
                 
-                % TODO missing vars
-                % Ferror(m-1)
-                
                 % Set up vars
                 XF = zeros(2,2);
-                XF(1,1) = sqrt(lambda*Fpower(m-1));
+                XF(1,1) = sqrt(lambda*obj.Ferrord(m-1));
                 XF(1,2) = Ferror(m-1);
-                XF(2,1) = sqrt(lambda)*conj(pb(m-1));
-                XF(2,2) = Berror(m-1);
+                XF(2,1) = sqrt(lambda)*conj(obj.pbd(m-1));
+                XF(2,2) = obj.Berrord(m-1);
                 
-                Fpower_new(m-1) = lambda*Fpower(m-1) + Ferror(m-1)*conj(Ferror(m-1));
+                Fpower(m-1) = lambda*obj.Ferrord(m-1) ...
+                    + Ferror(m-1)*conj(Ferror(m-1));
                 
-                cf = sqrt(lambda*Fpower(m-1))/sqrt(Fpower_new(m-1));
-                sf = Ferror(m-1)/sqrt(Fpower_new(m-1));
+                cf = sqrt(lambda*obj.Ferrord(m-1))/sqrt(Fpower(m-1));
+                sf = Ferror(m-1)/sqrt(Fpower(m-1));
                 PhiF = [ cf -sf; conj(sf) cf];
                 
                 % Compute
                 ZF = XF*PhiF;
                 
                 % Extract vars
-                pb_new(m-1) = ZF(2,1);
+                pb(m-1) = conj(ZF(2,1));
                 Berror(m) = ZF(2,2);
-                Fpower = Fpower_new; % do whole vector outside of loop?
                 
                 
             end
+            
+            % Save vars for next update
+            obj.pbd = pb; % i could overwrite pb
+            obj.pfd = pf; % i could overwrite pf
+            obj.Berrord = Berror;
+            obj.Bpowerdd = Bpowerd;
+            obj.Fpowerd = Fpower;
             
             
         end
