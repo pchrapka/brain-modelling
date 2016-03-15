79512592829bf1db0846e0af8380793356ccf8f2
pchrapka <pchrapka@gmail.com>
2016-03-15 14:31:51 -0400

    update external

---
+++ external/fieldtrip-beamforming/TODO.md
@@ -0,0 +1,3 @@
+# TODO
+
+Create artifact class, that contains flag describing if it's a processed/generated file or loaded file
--- external/lumberjack/+lumberjack/researchnb_savefig.m
+++ external/lumberjack/+lumberjack/researchnb_savefig.m
@@ -1,12 +1,15 @@
-function researchnb_savefig(name)
-% RESEARCHNB_SAVEFIG Saves the current figure to my research notebook
+function researchnb_savefig(filename)
+%RESEARCHNB_SAVEFIG Saves the current figure to my research notebook
+%   RESEARCHNB_SAVEFIG(filename) Saves the current figure to my research notebook
 %
-%   name
-%       file name
+%   Input
+%   -----
+%   filename (string)
+%       file name for image
 
 cfg = [];
 cfg.out_dir = fullfile('/home','phil','projects','research-notebook','img');
-cfg.file_name = [datestr(now, 'yyyy-mm-dd') '-' name];
+cfg.file_name = [datestr(now, 'yyyy-mm-dd') '-' filename];
 
 set(gcf, 'Color', 'w');
 lumberjack.save_figure(cfg);
