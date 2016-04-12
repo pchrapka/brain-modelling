7eb0c958c7c2f22acd67d71fa55da61289aafa97
pchrapka <pchrapka@gmail.com>
2016-04-12 15:43:31 -0400

    added iterations over order and number of channels

--- experiments/exp13-lsl-error-vs-size/exp13_lsl_error_vs_size.m
+++ experiments/exp13-lsl-error-vs-size/exp13_lsl_error_vs_size.m
@@ -2,39 +2,73 @@
 %   Goal:
 %   Test error of MQRDLSL algorithm as a function of number of parameters
 
-order = 2;
-nchannels = 4;
+order = [2,4,6,8,10];
+channels = [2,4,6,8,10,12,14];
 
-%% generate VAR
-s = VAR(nchannels, order);
-s.coefs_gen();
+norder = length(order);
+nchannels = length(channels);
 
-% simulate data
-nsamples = 1000;
-[X,X_norm,noise] = s.simulate(nsamples);
+% allocate mem
+results = [];
+results.ms_inno_error = zeros(nchannels,norder);
+results.rev = zeros(nchannels,norder);
+for i=1:nchannels
+    for j=1:norder
+        
+        %% generate VAR
+        s = VAR(channels(i), order(j));
+        s.coefs_gen();
+        
+        % simulate data
+        nsamples = 1000;
+        [X,X_norm,noise] = s.simulate(nsamples);
+        
+        %% Estimate coefs using lattice filter
+        
+        % channels from above
+        % order from above
+        lambda = 0.99;
+        filter = MQRDLSL2(channels(i),order(j),lambda);
+        trace = LatticeTrace(filter,'fields',{'Kf','ferror'});
+        
+        % run the filter
+        trace.run(X,'verbosity',0);
+        
+        %% Calculate the relative error
+        % Section 3.3 in Schlogl2000
+        
+        % innovation error Lewis1990
+        inno_error = (trace.trace.ferror(:,:,end)' - noise).^2;
+        ms_inno_error = sum(inno_error(:))/numel(inno_error);
+        
+        % relative error variance
+        % section 3.3 in Schlogl2000
+        ms_signal = var(X(:));
+        % all iterations, all channels, last order
+        last_ferror = trace.trace.ferror(:,:,end);
+        ms_pred_error = var(last_ferror(:));
+        rev = ms_pred_error/ms_signal;
+        
+        % save results
+        results.ms_inno_error(i,j) = ms_inno_error;
+        results.rev(i,j) = rev;
+    end
+    
+end
 
-%% Estimate coefs using lattice filter
+%% Plots
+figure;
+surf(results.rev);
+title('Relative Error Variance');
+zlabel('REV');
+xlabel('Number of channels');
+ylabel('Order');
+% FIXME This is showing a strange pattern, decreasing for increasing
+% channel number
 
-% lattice = [];
-
-% nchannels from above
-% order from above
-lambda = 0.99;
-verbose = 1;
-% % lattice(i).alg = MQRDLSL1(nchannels,order,lambda);
-% lattice.alg = MQRDLSL2(nchannels,order,lambda);
-% lattice.scale = 1;
-% lattice.name = sprintf('MQRDLSL C%d P%d lambda=%0.2f',nchannels,order,lambda);
-% 
-% % estimate the reflection coefficients
-% lattice = estimate_reflection_coefs(lattice, X, verbose);
-
-filter = MQRDLSL2(nchannels,order,lambda);
-trace = LatticeTrace(filter,'fields',{'Kf','ferror'});
-
-% run the filter
-trace.run(X,'verbosity',0);
-
-%% Calculate the relative error
-% Section 3.3 in Schlogl2000
-
+figure;
+surf(results.ms_inno_error);
+title('Innovation Error');
+zlabel('MSE');
+xlabel('Number of channels');
+ylabel('Order');
