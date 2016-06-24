1db3c99f74b09de5d253a0daf3e4e0b6f950f44c
pchrapka <pchrapka@gmail.com>
2016-06-24 14:29:20 -0400

    fixed up some bugs

--- experiments/exp23-explore-beamformed-data/exp23_explore_bf_data_diff.m
+++ experiments/exp23-explore-beamformed-data/exp23_explore_bf_data_diff.m
@@ -1,5 +1,8 @@
 %% exp23_explore_bf_data_diff
 
+clear all;
+close all;
+
 data_file = {};
 data_file{1} = '../output-common/fb/MRIstd-HMstd-cm-EP022-9913-L1cm-norm-tight-EEGstd-BPatchTriallcmvmom/sourceanalysis.mat';
 data_file{2} = '../output-common/fb/MRIstd-HMstd-cm-EP022-9913-L1cm-norm-tight-EEGodd-BPatchTriallcmvmom/sourceanalysis.mat';
@@ -26,8 +29,8 @@ for k=1:length(data_file)
     end
     
     % take mean and var
-    result{k}.sources_mean = squeeze(mean(sources,1));
-    result{k}.sources_var = squeeze(var(sources,1));
+    result{k}.sources_mean = squeeze(mean(result{k}.sources,1));
+    result{k}.sources_var = squeeze(var(result{k}.sources,1));
     
 end
 
@@ -35,24 +38,30 @@ end
 sources_mean_diff = result{1}.sources_mean - result{2}.sources_mean;
 sources_var_diff = result{1}.sources_var + result{2}.sources_var;
 
-overlay_variance = false;
-
 %% plot
-[ntrials,nsources,ntime] = size(sources);
+
+[ntrials,nsources,ntime] = size(result{1}.sources);
 ncols = 1;
 nrows = nsources;
+figure;
+for k=1:2
+    for i=1:nsources
+        subaxis(nrows, ncols, i,...
+            'Spacing', 0, 'SpacingVert', 0, 'Padding', 0, 'Margin', 0.05);
+        
+        hold on;
+        plot(result{k}.sources_mean(i,:));
+        
+        set(gca,'xticklabel',[]);
+        xlim([1 ntime]);
+    end
+end
+
+figure;
 for i=1:nsources
     subaxis(nrows, ncols, i,...
         'Spacing', 0, 'SpacingVert', 0, 'Padding', 0, 'Margin', 0.05);
     
-    if overlay_variance
-        h = plot_mean_and_var(1:ntime, sources_mean_diff(i,:), sources_var_diff(i,:),'b');
-        for j=1:length(h)
-            set(h(j),'FaceAlpha',0.3);
-            set(h(j),'EdgeColor','None');
-        end
-        hold on;
-    end
     plot(sources_mean_diff(i,:),'-b');
     
     set(gca,'xticklabel',[]);
