103df7cc73ceb34b37c849797cf0a5dc70727388
pchrapka <pchrapka@gmail.com>
2016-06-25 13:49:58 -0400

    updated exp10

--- experiments/exp10-beamform-lsl-svmmrmr/exp10_beamform_patch.m
+++ experiments/exp10-beamform-lsl-svmmrmr/exp10_beamform_patch.m
@@ -1,48 +1,9 @@
 %% exp10_beamform_patch
 
-do_run = false;
+analysis = build_analysis_beamform_patch();
 
-%% Analysis params
-
-% subject specific info
-[datadir,subject_file,subject_name] = get_coma_data(22);
-
-% set up output folder
-% use absolute directories
-[srcdir,~,~] = fileparts(mfilename('fullpath'));
-
-% use folder common to all experiments to avoid recomputation
-out_folder = fullfile(srcdir,'..','output-common','fb');
-if ~exist(out_folder,'dir')
-    mkdir(out_folder);
-end
-
-%% Beamform data
-analysis = {};
-
-% std
-stimulus = 'std';
-analysis{1} = analysis_eeg_beamform_patch(...
-    out_folder,...
-    'datadir', datadir,...
-    'subject_file', subject_file,...
-    'subject_name', subject_name,...
-    'stimulus', stimulus);
-
-if do_run
-    analysis{1}.process();
-end
-
-stimulus = 'odd';
-analysis{2} = analysis_eeg_beamform_patch(...
-    out_folder,...
-    'datadir', datadir,...
-    'subject_file', subject_file,...
-    'subject_name', subject_name,...
-    'stimulus', stimulus);
-
-if do_run
-    analysis{2}.process();
+for i=1:length(analysis)
+    analysis{i}.process();
 end
 
 %% Print beamformer output files
