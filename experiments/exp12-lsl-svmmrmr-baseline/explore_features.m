d04ac74a21f4a23f24d1ce12a96fb4a0794afe43
pchrapka <pchrapka@gmail.com>
2016-07-19 13:15:36 -0400

    updated exp12 to new format

---
+++ experiments/exp12-lsl-svmmrmr-baseline/explore_features.m
@@ -0,0 +1,24 @@
+%% explore_features
+
+% code to run analysis pipeline
+% pipeline = build_pipeline_lattice_svm('params_sd_22');
+% pipeline.run();
+
+params_subject = 'params_sd_22';
+params_name = {...
+    'params_lf_MQRDLSL2_p10_l099_n400',...
+    };
+
+explore_features_lattice_svm(params_subject,params_name);
+
+%% 
+% code to run analysis pipeline
+% pipeline = build_pipeline_lattice_svm('params_sd_22_consec');
+% pipeline.run();
+
+params_subject = 'params_sd_22_consec';
+params_name = {...
+    'params_lf_MQRDLSL2_p10_l099_n400',...
+    };
+
+explore_features_lattice_svm(params_subject,params_name);
