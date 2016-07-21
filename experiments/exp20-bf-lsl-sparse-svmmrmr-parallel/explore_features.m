47e18992497b7350ee367816b90936ba4e417d80
pchrapka <pchrapka@gmail.com>
2016-07-21 09:09:44 -0400

    fix up explore features scripts

--- experiments/exp20-bf-lsl-sparse-svmmrmr-parallel/explore_features.m
+++ experiments/exp20-bf-lsl-sparse-svmmrmr-parallel/explore_features.m
@@ -1,65 +1,25 @@
 %% explore_features
 
-test = false;
-
 % code to run analysis pipeline
 % pipeline = build_pipeline_lattice_svm('params_sd_22');
 % pipeline.run();
 
 params_subject = 'params_sd_22';
-params_name = {'params_lf_MLOCCDTWL_p10_l099_n400'};
+params_name = {...
+    'params_lf_MLOCCDTWL_p10_l099_n400',...
+    };
 
-print_results_lattice_svm(params_subject,params_name,'tofile',true);
+explore_features_lattice_svm(params_subject,params_name);
 
-% get pipeline
-pipeline = build_pipeline_lattice_svm('params_sd_22');
+%% 
+% code to run analysis pipeline
+% pipeline = build_pipeline_lattice_svm('params_sd_22_consec');
+% pipeline.run();
 
-% select jobs based on filter params
-brick_name = 'bricks.lattice_filter_sources';
-brick_code = pipeline.get_brick_code(brick_name);
-params_name = 'params_lf_MLOCCDTWL_p10_l099_n400';
-param_code = pipeline.get_params_code(brick_name,params_name);
+params_subject = 'params_sd_22_consec';
+params_name = {...
+    'params_lf_MLOCCDTWL_p10_l099_n400',...
+    'params_lf_MLOCCDTWL_p10_l098_n400',...
+    };
 
-% select feature validation jobs
-brick_name = 'bricks.features_validate';
-brick_code_desired = pipeline.get_brick_code(brick_name);
-
-pattern = ['.+' brick_code param_code '.*' brick_code_desired '\d+\>'];
-jobs = fieldnames(pipeline.pipeline);
-job_idx = cellfun(@(x) ~isempty(regexp(x,pattern,'match')),jobs,'UniformOutput',true);
-jobs_desired = jobs(job_idx);
-
-for i=1:length(jobs_desired)
-    
-    % decrypt job code
-    pattern = [brick_code_desired '\d+\>'];
-    job_code = regexp(jobs_desired{i},pattern,'match');
-    job_name = pipeline.expand_code(job_code{1},'expand','params');
-    
-    fprintf('%s\n',job_name);
-    fprintf('%s\n',repmat('-',1,length(job_name)));
-    
-    file_validated = pipeline.pipeline.(jobs_desired{i}).files_out;
-    fprintf('file: %s\n',file_validated);
-    
-    if ~test
-        % load data
-        din = load(file_validated);
-        
-        % select common features
-        ncommon = 10;
-        [feat_common, freq] = features_select_common(din.data.feat_sel,ncommon);
-        
-        % select corresponding labels
-        labels_mrmr = din.data.class_labels(feat_common);
-        
-        fprintf(' Index     | Frequency | Label     \n');
-        fprintf('-----------------------------------\n');
-        for j=1:ncommon
-            fprintf(' %9d | %9d | %s\n',feat_common(j), freq(j), labels_mrmr{j});
-        end
-    end
-    
-    fprintf('\n');
-    
-end
+explore_features_lattice_svm(params_subject,params_name);
