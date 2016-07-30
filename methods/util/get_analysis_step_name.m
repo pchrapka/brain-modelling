051282305564b292998152386866fa9d299a8aee
pchrapka <pchrapka@gmail.com>
2016-07-30 12:55:53 -0400

    updated helper function

--- methods/util/get_analysis_step_name.m
+++ methods/util/get_analysis_step_name.m
@@ -1,7 +1,11 @@
 function step_name = get_analysis_step_name(param_file, prefix)
 
-pattern = [prefix '(.*).mat'];
-results = regexp(param_file,pattern,'tokens');
-step_name = results{1}{1};
+if ischar(param_file)
+    pattern = [prefix '(.*).mat'];
+    results = regexp(param_file,pattern,'tokens');
+    step_name = results{1}{1};
+else
+    step_name = param_file.name;
+end
 
 end
