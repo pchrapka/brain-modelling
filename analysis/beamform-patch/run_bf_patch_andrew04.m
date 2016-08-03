bc827d8eb6dc37dd0566a49031e924d6028b3dc6
pchrapka <pchrapka@gmail.com>
2016-08-03 11:36:33 -0400

    added plotting code

--- analysis/beamform-patch/run_bf_patch_andrew04.m
+++ analysis/beamform-patch/run_bf_patch_andrew04.m
@@ -3,3 +3,17 @@
 %% pipeline so far
 pipeline = build_pipeline_beamformer('paramsbf_sd_andrew04');
 pipeline.process();
+
+%% visual checks
+
+do_plots = false;
+
+if do_plots
+    % electrodes pre-alignment
+    figure;
+    obj_elec.plot({'fiducials','scalp','electrodes','electrodes-labels'});
+    
+    % electrodes post-alignment
+    figure;
+    obj_elec.plot({'fiducials','scalp','electrodes-aligned','electrodes-labels'});
+end
