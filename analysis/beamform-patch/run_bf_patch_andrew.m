ec1701c200d33336d6f5a5f1d28a1965ab156646
pchrapka <pchrapka@gmail.com>
2016-08-04 12:43:37 -0400

    some more reorganization

---
+++ analysis/beamform-patch/run_bf_patch_andrew.m
@@ -0,0 +1,25 @@
+%% run_bf_patch_andrew
+
+%% standard trials
+pipeline = build_pipeline_beamformer(paramsbf_sd_andrew(6,10,'std')); 
+pipeline.process();
+
+%% visual checks
+
+do_plots = true;
+
+if do_plots
+    obj_elec = pipeline.steps{end}.get_dep('ftb.Electrodes');
+    
+    % electrodes pre-alignment
+    figure;
+    obj_elec.plot({'fiducials','scalp','electrodes','electrodes-labels'});
+    
+    % electrodes post-alignment
+    figure;
+    obj_elec.plot({'fiducials','scalp','electrodes-aligned','electrodes-labels'});
+end
+
+%% deviant trials
+% pipeline = build_pipeline_beamformer(paramsbf_sd_andrew(6,10,'odd'));
+% pipeline.process();
