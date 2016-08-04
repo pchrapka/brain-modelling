ead75e49ab50e8e4dc4f40c0729c0e66525dcc65
pchrapka <pchrapka@gmail.com>
2016-08-04 13:35:58 -0400

    updated ftb

--- external/fieldtrip-beamforming/configs/BFPatchAAL13.m
+++ external/fieldtrip-beamforming/configs/BFPatchAAL13.m
@@ -1,13 +1,12 @@
 function BFPatchAAL13()
 % BFPatchAAL13
 
-[srcdir,~,~] = fileparts(mfilename('fullpath'));
-
 cfg = [];
 cfg.cortical_patches_name = 'aal-coarse-13';
 cfg.ft_sourceanalysis.method = 'lcmv';
 cfg.ft_sourceanalysis.lcmv.keepmom = 'yes';
 
+[srcdir,~,~] = fileparts(mfilename('fullpath'));
 save(fullfile(srcdir, [strrep(mfilename,'_','-') '.mat']),'cfg');
 
 end
