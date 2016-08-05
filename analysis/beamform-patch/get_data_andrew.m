dae64ad0e1018c1d9b06cfe083ea48259d8f6c82
pchrapka <pchrapka@gmail.com>
2016-08-05 14:20:15 -0400

    added a check to see if the data is available fixed electrode labeling issue, eeg and electrode data are not the same

--- analysis/beamform-patch/get_data_andrew.m
+++ analysis/beamform-patch/get_data_andrew.m
@@ -16,8 +16,37 @@ end
 
 % set up outputs
 data_dir = fullfile(rootdir,'projects','data-andrew-beta');
+if ~exist(data_dir,'dir')
+    error([mfilename ':MissingData'],...
+        'cannot find data-andrew-beta');
+end
 data_file = fullfile(data_dir,sprintf('exp%02d_%d.bdf',subject_num,deviant_percent));
-elec_file = fullfile(data_dir,sprintf('exp%02d.sfp',subject_num));
+elec_file = fullfile(data_dir,sprintf('exp%02d_mod.sfp',subject_num));
+
+if ~exist(elec_file,'file')
+    % remove the hyphen in the electrode file to match the eeg header
+    
+    elec_file_orig = fullfile(data_dir,sprintf('exp%02d.sfp',subject_num));
+    % open the original electrode file
+    fid = fopen(elec_file_orig);
+    % read the data
+    elec_data = textscan(fid, '%s%f%f%f');
+    fclose(fid);
+    
+    % remove the hyphen
+    channels = cellfun(@(x) strrep(x,'-',''), elec_data{1},'UniformOutput',false);
+    % save the new channel names
+    elec_data{1} = channels;
+    
+    % write to a new file
+    fid = fopen(elec_file,'w');
+    nchannels = length(elec_data{1});
+    for i=1:nchannels
+        fprintf(fid,'%s\t%0.4f\t%0.4f\t%0.4f\n',...
+            elec_data{1}{i},elec_data{2}(i),elec_data{3}(i),elec_data{4}(i));
+    end
+    fclose(fid);
+end
 
 data_name = sprintf('s%02d-%d',subject_num,deviant_percent);
 
