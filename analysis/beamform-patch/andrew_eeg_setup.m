fa2e2663189eaf8ed75fee460649c10f77309ddf
pchrapka <pchrapka@gmail.com>
2016-08-05 14:20:23 -0400

    added some debugging code

--- analysis/beamform-patch/andrew_eeg_setup.m
+++ analysis/beamform-patch/andrew_eeg_setup.m
@@ -14,3 +14,28 @@ cfg = [];
 cfg.dataset = data_file;
 cfg.trialdef.eventtype = '?';
 cfg = ft_definetrial(cfg);
+
+%% check single trial stuff
+
+pipeline = build_pipeline_beamformer(paramsbf_sd_andrew(6,10,'std'));
+
+eeg_obj = pipeline.steps{end}.get_dep('ftb.EEG');
+
+%%
+cfg = [];
+cfg.covariance = 'yes';
+cfg.covariancewindow = 'all';
+cfg.keeptrials = 'no';
+cfg.removemean = 'yes';
+
+data_pre = loadfile(eeg_obj.preprocessed);
+timelock = ft_timelockanalysis(cfg,data_pre);
+
+%%
+lf_obj = pipeline.steps{end}.get_dep('ftb.Leadfield');
+grid = loadfile(lf_obj.leadfield);
+
+cfg = [];
+cfg.method = 'lcmv';
+cfg.grid = grid;
+source = ft_sourceanalysis(cfg,timelock);
