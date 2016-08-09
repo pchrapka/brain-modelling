51b8ba4cd2afdd53ab7029604cb593664a5710b9
pchrapka <pchrapka@gmail.com>
2016-08-09 14:55:30 -0400

    refactor performance vs trials script

--- experiments/exp30-explore-tvar/exp30_tvar_vs_nchannels.m
+++ experiments/exp30-explore-tvar/exp30_tvar_vs_nchannels.m
@@ -1,14 +1,6 @@
-% exp30_tvar_vs_nchannels
+%% exp30_tvar_vs_nchannels
 
-[srcdir,func_name,~] = fileparts(mfilename('fullpath'));
-outdir = fullfile(srcdir,'output');
-if ~exist(outdir,'dir')
-    mkdir(outdir);
-end
-
-setup_parfor();
-
-%% set up params
+%% set options
 nsims = 20;
 channels = [2 4 6 8 10 12 14 16];
 % channels = [2 4];
@@ -19,6 +11,9 @@ lambda = 0.98;
 
 verbosity = 0;
 
+%% set up benchmark params and run
+
+%% MCMTQRDLSL1
 sim_params = [];
 for k=1:nchannel_opts
     nchannels = channels(k);
@@ -31,13 +26,14 @@ run_lattice_benchmark(...
     mfilename('fullpath'),...
     'name',sim_params(1).filter.name,...
     'sim_params', sim_params,...
-    'nsims', 20,...
+    'nsims', nsims,...
     'noise_warmup', true,...
     'plot_avg_mse', true,...
     'plot_avg_nmse', true);
 
 close all;
 
+%% MQRDLSL1
 sim_params = [];
 for k=1:nchannel_opts
     nchannels = channels(k);
@@ -50,13 +46,14 @@ run_lattice_benchmark(...
     mfilename('fullpath'),...
     'name',sim_params(1).filter.name,...
     'sim_params', sim_params,...
-    'nsims', 20,...
+    'nsims', nsims,...
     'noise_warmup', true,...
     'plot_avg_mse', true,...
     'plot_avg_nmse', true);
 
 close all;
 
+%% MQRDLSL2
 sim_params = [];
 for k=1:nchannel_opts
     nchannels = channels(k);
@@ -69,13 +66,14 @@ run_lattice_benchmark(...
     mfilename('fullpath'),...
     'name',sim_params(1).filter.name,...
     'sim_params', sim_params,...
-    'nsims', 20,...
+    'nsims', nsims,...
     'noise_warmup', true,...
     'plot_avg_mse', true,...
     'plot_avg_nmse', true);
 
 close all;
 
+%% MLOCCD_TWL
 sim_params = [];
 ntime = 358;
 sigma = 10^(-1);
@@ -92,7 +90,7 @@ run_lattice_benchmark(...
     mfilename('fullpath'),...
     'name',sim_params(1).filter.name,...
     'sim_params', sim_params,...
-    'nsims', 20,...
+    'nsims', nsims,...
     'noise_warmup', true,...
     'plot_avg_mse', true,...
     'plot_avg_nmse', true);
@@ -100,214 +98,3 @@ run_lattice_benchmark(...
 close all;
 
 
-% k=1;
-% filters = [];
-% filters(k).name = 'MQRDLSL1';
-% filters(k).params = {'order',order_est,'lambda',lambda,'ntrials',1};
-% k = k+1;
-% 
-% filters(k).name = 'MQRDLSL2';
-% filters(k).params = {'order',order_est,'lambda',lambda,'ntrials',1};
-% k = k+1;
-% 
-% filters(k).name = 'MCMTQRDLSL1';
-% filters(k).params = {'order',order_est,'lambda',lambda,'ntrials',5};
-% k = k+1;
-% 
-% filters(k).name = 'MLOCCDTWL';
-% filters(k).params = {'order',order_est,'lambda',lambda,'ntrials',1};
-% k = k+1;
-% 
-% 
-% plot_individual = false;
-% plot_coef_values = false;
-% 
-% %% loop over params
-% 
-% large_error = zeros(nchannel_opts,nsims);
-% fresh = false(nchannel_opts,nsims);
-% plotted_coef_values = false(nchannel_opts); % reset flag
-% for k=1:length(filters)
-%     % allocate mem
-%     labels = cell(nchannel_opts,1);
-%     data_args = [];
-% 
-%     filter_type = filters(k);
-%     for i=1:nchannel_opts
-%         nchannels = channels(i);
-%         
-%         trace = cell(nsims,1);
-%         estimate = cell(nsims,1);
-%         kf_true_sims = cell(nsims,1);
-%         data_sim = cell(nsims,1);
-%         
-%         %for j=1:nsims
-%         parfor j=1:nsims
-%             
-%             % set up output file for sim data
-%             slug_sim = sprintf('vrc-tvar-p%d-c%d-s%d',order_est,nchannels,j);
-%             outfile_sim = fullfile(outdir,[slug_sim '.mat']);
-%             
-%             if ~exist(outfile_sim,'file')
-%                 fprintf('simulating: %s\n', slug_sim);
-%                 
-%                 % generate data
-%                 data = exp30_gen_tvar(nchannels);
-%                 % save data
-%                 save_parfor(outfile_sim,data);
-%                 
-%                 % copy data for filtering
-%                 kf_true_sims{j} = data.true;
-%                 data_sim{j} = data;
-%                 
-%                 % set flag that the simulated data is new
-%                 fresh(i,j) = true;
-%             else
-%                 fprintf('loading: %s\n', slug_sim);
-%                 
-%                 % load data
-%                 data = loadfile(outfile_sim);
-%                 
-%                 % copy data for filtering
-%                 kf_true_sims{j} = data.true;
-%                 data_sim{j} = data;
-%             end
-%         end
-%         
-%         % set up filter slug
-%         [filter_main,~] = exp30_get_filter(filter_type.name,...
-%             'nchannels',nchannels,...
-%             filter_type.params{:});
-%         
-%         slug_filter = filter_main.name;
-%         slug_filter = strrep(slug_filter,' ','-');
-%         
-%         parfor j=1:nsims
-%             %for j=1:nsims
-%             slug_sim = sprintf('vrc-tvar-p%d-c%d-s%d',order_est,nchannels,j);
-%             slug_sim_filt = sprintf('%s-%s',slug_sim,slug_filter);
-%             outfile = fullfile(outdir,[slug_sim_filt '.mat']);
-%             
-%             if fresh(i,j) || ~exist(outfile,'file')
-%                 fprintf('running: %s\n', slug_sim_filt)
-%                 sources = data_sim{j}.signal;
-%                 
-%                 [filter,mt] = exp30_get_filter(filter_type.name,...
-%                     'nchannels',nchannels,...
-%                     filter_type.params{:});
-%                 trace{j} = LatticeTrace(filter,'fields',{'Kf'});
-%                 
-%                 ntime = size(sources,2);
-%                 mu = zeros(nchannels,1);
-%                 sigma = eye(nchannels);
-%                 noise = zeros(nchannels,ntime,mt);
-%                 for m=1:mt
-%                     noise(:,:,m) = mvnrnd(mu,sigma,ntime)';
-%                 end
-%                 trace_noise = LatticeTrace(filter,'fields',{'Kf'});
-%                 
-%                 warning('off','all');
-%                 % run filter on noise
-%                 trace_noise.run(noise,'verbosity',verbosity,'mode','none');
-%                 
-%                 % run the filter on data
-%                 trace{j}.run(sources(:,:,1:mt),'verbosity',verbosity,'mode','none');
-%                 warning('on','all');
-%                 
-%                 trace{j}.name = trace{j}.filter.name;
-%                 
-%                 estimate{j} = trace{j}.trace.Kf;
-%                 
-%                 % save data
-%                 data = [];
-%                 data.estimate = estimate{j};
-%                 save_parfor(outfile,data);
-%             else
-%                 fprintf('loading: %s\n', slug_sim_filt);
-%                 % load data
-%                 data = loadfile(outfile);
-%                 estimate{j} = data.estimate;
-%             end
-%             
-%             data_mse = mse_iteration(estimate{j},kf_true_sims{j});
-%             if any(data_mse > 10)
-%                 large_error(i,j) = large_error(i,j) + 1;
-%             end
-%         end
-%         
-%         % plot MSE for each sim
-%         if plot_individual
-%             for j=1:nsims
-%                 h = figure;
-%                 clf;
-%                 plot_mse_vs_iteration(...
-%                     estimate{j}, kf_true_sims{j},...
-%                     'mode','log',...
-%                     'labels',{sprintf('%d channels',nchannels)});
-%                 drawnow;
-%                 save_fig_exp(mfilename('fullpath'),'tag',sprintf('mse-%s-s%d',slug_filter,j));
-%                 close(h);
-%             end
-%         end
-%         
-%         % plot unique reflection coefficient values
-%         if plot_coef_values
-%             if ~plotted_coef_values(i)
-%                 for j=1:nsims
-%                     h = figure;
-%                     clf;
-%                     kf_unique = unique(kf_true_sims{j});
-%                     hist(kf_unique,linspace(-1,1,20));
-%                     drawnow;
-%                     save_fig_exp(mfilename('fullpath'),...
-%                         'tag',sprintf('coefs-c%d-s%d',channels(i),j),...
-%                         'formats',{'png'});
-%                     close(h);
-%                 end
-%                 plotted_coef_values(i) = true; % do this only once per channel
-%             end
-%         end
-%         
-%         data_args = [data_args {estimate kf_true_sims}];
-%         labels{i} = sprintf('%d channels',nchannels);
-%     end
-%     
-%     %% Plot MSE
-%     
-%     h = figure;
-%     clf;
-%     plot_mse_vs_iteration(...
-%         data_args{:},...
-%         'mode','log',...
-%         'labels',labels);
-%     drawnow;
-%     save_fig_exp(mfilename('fullpath'),'tag',sprintf('mse-all-%s',slug_filter));
-%     ylim([10^(-4) 10^(0)]);
-%     drawnow;
-%     save_fig_exp(mfilename('fullpath'),'tag',sprintf('mse-all-axis-%s',slug_filter));
-%     
-%     h = figure;
-%     clf;
-%     plot_mse_vs_iteration(...
-%         data_args{:},...
-%         'mode','log',...
-%         'normalized',true,...
-%         'labels',labels);
-%     drawnow;
-%     save_fig_exp(mfilename('fullpath'),'tag',sprintf('nmse-all-%s',slug_filter));
-%     ylim([10^(-1) 10^(3)]);
-%     drawnow;
-%     save_fig_exp(mfilename('fullpath'),'tag',sprintf('nmse-all-axis-%s',slug_filter));
-% end
-% 
-% %% Print extra info
-% fprintf('large errors\n');
-% for i=1:nchannel_opts
-%     nchannels = channels(i);
-%     for j=1:nsims
-%         if large_error(i,j) > 0
-%             fprintf('\tc%d-s%d = %d/%d\n',nchannels,j,large_error(i,j),length(filters));
-%         end
-%     end
-% end
-% 
--- experiments/exp30-explore-tvar/exp30_tvar_vs_ntrials.m
+++ experiments/exp30-explore-tvar/exp30_tvar_vs_ntrials.m
@@ -1,16 +1,8 @@
 %% exp30_tvar_vs_ntrials
 
-[srcdir,func_name,~] = fileparts(mfilename('fullpath'));
-outdir = fullfile(srcdir,'output');
-if ~exist(outdir,'dir')
-    mkdir(outdir);
-end
-
-setup_parfor();
-
-%% set up params
+%% set options
 nsims = 20;
-trials = [2 3 5 8 13 21 34 55 89];
+trials = [89 2 3 5 8 13 21 34 55];
 ntrials_opts = length(trials);
 ntrials_max = max(trials);
 
@@ -22,223 +14,28 @@ lambda = 0.98;
 
 verbosity = 0;
 
-k = 1;
-filters = [];
+%% set up benchmark params and run
+for k=1:nchannel_opts
+    nchannels = channels(k);
 
-filters(k).name = 'MCMTQRDLSL1';
-filters(k).params = {'order',order_est,'lambda',lambda};
-k = k+1;
-
-plot_individual = false;
-plot_coef_values = false;
-
-%% loop over params
-large_error = zeros(nchannel_opts,ntrials_opts,nsims);
-for z=1:nchannel_opts
-    nchannels = channels(z);
-    
-    fresh = false(ntrials_opts,nsims);
-    plotted_coef_values = false(ntrials_opts); % reset flag
-    
-    for k=1:length(filters)
-        % allocate mem
-        labels = cell(ntrials_opts,1);
-        data_args = [];
-        
-        filter_type = filters(k);
-        for i=1:ntrials_opts
-            ntrials = trials(i);
-            
-            trace = cell(nsims,1);
-            estimate = cell(nsims,1);
-            kf_true_sims = cell(nsims,1);
-            data_sim = cell(nsims,1);
-            
-            %for j=1:nsims
-            parfor j=1:nsims
-                
-                % set up output file for sim data
-                slug_sim = sprintf('vrc-tvar-p%d-c%d-s%d',order_est,nchannels,j);
-                outfile_sim = fullfile(outdir,[slug_sim '.mat']);
-                
-                if ~exist(outfile_sim,'file')
-                    fprintf('simulating: %s\n', slug_sim);
-                    
-                    % generate data
-                    data = exp30_gen_tvar(nchannels,'ntrials',ntrials_max);
-                    % save data
-                    save_parfor(outfile_sim,data);
-                    
-                    % copy data for filtering
-                    kf_true_sims{j} = data.true;
-                    data_sim{j} = data;
-                    
-                    % set flag that the simulated data is new
-                    fresh(i,j) = true;
-                else
-                    fprintf('loading: %s\n', slug_sim);
-                    
-                    % load data
-                    data = loadfile(outfile_sim);
-                    
-                    % check how many trials there are
-                    ntrials_data = size(data.signal,3);
-                    if ntrials_data < ntrials_max
-                        % generate more trials if there aren't enough
-                        data = exp30_gen_tvar(nchannels,...
-                            'ntrials', ntrials_max, 'process', data.process);
-                        save_parfor(outfile_sim,data);
-                    end
-                    
-                    % copy data for filtering
-                    kf_true_sims{j} = data.true;
-                    data_sim{j} = data;
-                end
-            end
-            
-            % set up filter slug
-            [filter_main,~] = exp30_get_filter(filter_type.name,...
-                'ntrials',ntrials,...
-                'nchannels',nchannels,...
-                filter_type.params{:});
-            
-            slug_filter = filter_main.name;
-            slug_filter = strrep(slug_filter,' ','-');
-            
-            parfor j=1:nsims
-                %for j=1:nsims
-                slug_sim = sprintf('vrc-tvar-p%d-c%d-s%d',order_est,nchannels,j);
-                slug_sim_filt = sprintf('%s-%s',slug_sim,slug_filter);
-                outfile = fullfile(outdir,[slug_sim_filt '.mat']);
-                
-                if fresh(i,j) || ~exist(outfile,'file')
-                    fprintf('running: %s\n', slug_sim_filt)
-                    sources = data_sim{j}.signal;
-                    
-                    [filter,~] = exp30_get_filter(filter_type.name,...
-                        'ntrials',ntrials,...
-                        'nchannels',nchannels,...
-                        filter_type.params{:});
-                    trace{j} = LatticeTrace(filter,'fields',{'Kf'});
-                    
-                    ntime = size(sources,2);
-                    mu = zeros(nchannels,1);
-                    sigma = eye(nchannels);
-                    noise = zeros(nchannels,ntime,ntrials);
-                    for m=1:ntrials
-                        noise(:,:,m) = mvnrnd(mu,sigma,ntime)';
-                    end
-                    trace_noise = LatticeTrace(filter,'fields',{'Kf'});
-                    
-                    warning('off','all');
-                    % run filter on noise
-                    trace_noise.run(noise,'verbosity',verbosity,'mode','none');
-                    
-                    % run the filter on data
-                    trace{j}.run(sources(:,:,1:ntrials),'verbosity',verbosity,'mode','none');
-                    warning('on','all');
-                    
-                    trace{j}.name = trace{j}.filter.name;
-                    
-                    estimate{j} = trace{j}.trace.Kf;
-                    
-                    % save data
-                    data = [];
-                    data.estimate = estimate{j};
-                    save_parfor(outfile,data);
-                else
-                    fprintf('loading: %s\n', slug_sim_filt);
-                    % load data
-                    data = loadfile(outfile);
-                    estimate{j} = data.estimate;
-                end
-                
-                data_mse = mse_iteration(estimate{j},kf_true_sims{j});
-                if any(data_mse > 10)
-                    large_error(z,i,j) = large_error(z,i,j) + 1;
-                end
-            end
-            
-            % plot MSE for each sim
-            if plot_individual
-                for j=1:nsims
-                    h = figure;
-                    clf;
-                    plot_mse_vs_iteration(...
-                        estimate{j}, kf_true_sims{j},...
-                        'mode','log',...
-                        'labels',{sprintf('%d channels',nchannels)});
-                    drawnow;
-                    save_fig_exp(mfilename('fullpath'),'tag',sprintf('mse-%s-s%d',slug_filter,j));
-                    close(h);
-                end
-            end
-            
-            % plot unique reflection coefficient values
-            if plot_coef_values
-                if ~plotted_coef_values(i)
-                    for j=1:nsims
-                        h = figure;
-                        clf;
-                        kf_unique = unique(kf_true_sims{j});
-                        hist(kf_unique,linspace(-1,1,20));
-                        drawnow;
-                        save_fig_exp(mfilename('fullpath'),...
-                            'tag',sprintf('coefs-c%d-s%d',channels(i),j),...
-                            'formats',{'png'});
-                        close(h);
-                    end
-                    plotted_coef_values(i) = true; % do this only once per channel
-                end
-            end
-            
-            data_args = [data_args {estimate kf_true_sims}];
-            labels{i} = sprintf('%d trials',ntrials);
-        end
-        
-        %% Plot MSE
-        
-        h = figure;
-        clf;
-        plot_mse_vs_iteration(...
-            data_args{:},...
-            'mode','log',...
-            'labels',labels);
-        drawnow;
-        save_fig_exp(mfilename('fullpath'),'tag',sprintf('mse-ntrials-%s',slug_filter));
-        ylim([10^(-4) 10^(0)]);
-        drawnow;
-        save_fig_exp(mfilename('fullpath'),'tag',sprintf('mse-ntrials-axis-%s',slug_filter));
-        
-        h = figure;
-        clf;
-        plot_mse_vs_iteration(...
-            data_args{:},...
-            'mode','log',...
-            'normalized',true,...
-            'labels',labels);
-        drawnow;
-        save_fig_exp(mfilename('fullpath'),'tag',sprintf('nmse-ntrials-%s',slug_filter));
-        ylim([10^(-1) 10^3]);
-        drawnow;
-        save_fig_exp(mfilename('fullpath'),'tag',sprintf('nmse-ntrials-axis-%s',slug_filter));
+    sim_params = [];
+    for j=1:ntrials_opts
+        ntrials = trials(j);
+        sim_params(j).filter = MCMTQRDLSL1(ntrials,nchannels,order_est,lambda);
+        sim_params(j).data = 'vrc-2ch-coupling';
+        sim_params(j).label = sprintf('%d trials',ntrials);
     end
     
-end
-
-%% Print extra info
-fprintf('large errors\n');
-for z=1:nchannel_opts
-    nchannels = channels(z);
+    % run 
+    run_lattice_benchmark(...
+        mfilename('fullpath'),...
+        'name',sim_params(1).filter.name,...
+        'sim_params', sim_params,...
+        'nsims', nsims,...
+        'noise_warmup', true,...
+        'plot_avg_mse', true,...
+        'plot_avg_nmse', true);
     
-    for i=1:ntrials_opts
-        ntrials = trials(i);
-        
-        for j=1:nsims
-            if large_error(z,i,j) > 0
-                fprintf('\tt%d-c%d-s%d = %d/%d\n',...
-                    ntrials,nchannels,j,large_error(z,i,j),length(filters));
-            end
-        end
-    end
+    close all;
+    
 end
