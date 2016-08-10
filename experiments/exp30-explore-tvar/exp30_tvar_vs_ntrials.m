e5e0349fa30ae89f0c87acc0f14164f61db9037c
pchrapka <pchrapka@gmail.com>
2016-08-09 21:38:37 -0400

    fix vrc generator

--- experiments/exp30-explore-tvar/exp30_tvar_vs_ntrials.m
+++ experiments/exp30-explore-tvar/exp30_tvar_vs_ntrials.m
@@ -14,6 +14,9 @@ lambda = 0.98;
 
 verbosity = 0;
 
+data_type = 'vrc-cp-ch2-coupling1-fixed';
+% data_type = 'vrc-cp-ch2-coupling2-rnd';
+
 %% set up benchmark params and run
 for k=1:nchannel_opts
     nchannels = channels(k);
@@ -22,7 +25,7 @@ for k=1:nchannel_opts
     for j=1:ntrials_opts
         ntrials = trials(j);
         sim_params(j).filter = MCMTQRDLSL1(ntrials,nchannels,order_est,lambda);
-        sim_params(j).data = 'vrc-2ch-coupling';
+        sim_params(j).data = data_type;
         sim_params(j).label = sprintf('%d trials',ntrials);
     end
     
