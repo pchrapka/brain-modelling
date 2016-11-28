8f3dbf6d0c1c6d908b137fea31ba8a85a82a2d75
pchrapka <pchrapka@gmail.com>
2016-11-28 13:40:13 -0500

    altered the test script a bit

--- bm_tests.m
+++ bm_tests.m
@@ -1,4 +1,4 @@
-%% runtests
+    %% runtests
 clc;
 close all;
 import matlab.unittest.TestSuite
@@ -6,34 +6,27 @@ import matlab.unittest.TestSuite
 verbosity = 1;
 
 %% Run all tests
-suite = TestSuite.fromPackage('tests');
-
-% Show tests
-if verbosity > 0
-    disp({suite.Name}');
-end
-result = run(suite);
+% suite = TestSuite.fromPackage('tests');
+% 
+% % Show tests
+% if verbosity > 0
+%     disp({suite.Name}');
+% end
+% result = run(suite);
 
 %% Included in all
 % Run some subsets
 
 % suite = TestSuite.fromClass(?tests.TestPipelineLatticeSVM);
-% if verbosity > 0
-%     disp({suite.Name}');
-% end
-% result = run(suite);
-
 % suite = TestSuite.fromClass(?tests.TestSVM);
-% if verbosity > 0
-%     disp({suite.Name}');
-% end
-% result = run(suite);
-
-% suite = TestSuite.fromClass(?tests.Test_plot_rc_feature_matrix_boxplot);
-% if verbosity > 0
-%     disp({suite.Name}');
-% end
-% result = run(suite);
+% suite = TestSuite.fromClass(?tests.Test_plot_rc_feature_matrix);
+% suite = TestSuite.fromClass(?tests.TestVAR);
+suite = TestSuite.fromClass(?tests.TestVRC);
+% suite = TestSuite.fromClass(?tests.TestFilters);
+if verbosity > 0
+    disp({suite.Name}');
+end
+result = run(suite);
 
 %% Not included in all
 % Test_ft_trialfun_preceed
