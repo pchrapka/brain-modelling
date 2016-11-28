3d4a7802451e06bdb21155b8c6b0ba56346e5b12
pchrapka <pchrapka@gmail.com>
2016-11-28 17:13:11 -0500

    fixed up filter testing function

--- methods/adaptive-filter/lattice_filter_test.m
+++ methods/adaptive-filter/lattice_filter_test.m
@@ -12,16 +12,31 @@ function lattice_filter_test(filter_name, varargin)
 %   name, value pairs
 %       filter constructor arguments
 
-K = 4;
-order = 3;
+p = inputParser();
+addRequired(p,'filter_name',@ischar);
+addParameter(p,'filter_args',{},@iscell);
+addParameter(p,'channels',4,@isnumeric);
+addParameter(p,'order',3,@isnumeric);
+addParameter(p,'samples',1000,@isnumeric);
+parse(p,filter_name,varargin{:});
 
-% test constructor
-process = VRC(K,order);
+switch filter_name 
+    case 'MCMTQRDLSL1'
+        ntrials = p.Results.filter_args{1};
+    otherwise
+        ntrials = 1;
+end
+
+% create process and data
+process = VRC(p.Results.channels,p.Results.order);
 process.coefs_gen_sparse('mode','exact','ncoefs',6);
-data = process.simulate(1000);
+data = zeros(p.Results.channels, p.Results.samples, ntrials);
+for i=1:ntrials
+    data(:,:,i) = process.simulate(p.Results.samples);
+end
 
 filterhandle = str2func(filter_name);
-filter = filterhandle(K,order,varargin{:});
+filter = filterhandle(p.Results.channels,p.Results.order,p.Results.filter_args{:});
 trace = LatticeTrace(filter,'fields',{'Kf','Kb'});
 
 trace.run(data,'verbosity',0,'mode','none');
@@ -30,9 +45,11 @@ result = isequalntol(squeeze(trace.trace.Kf(end,:,:,:)),...
     process.Kf, 'AbsTol', 0.3);
 idx = abs(process.Kf) > 0;
 accuracy_percent = sum(result(idx))/length(result(idx));
-fprintf('Kf is %d%% OK\n',accuracy_percent*100);
+fprintf('Kf is %0.0f%% OK\n',accuracy_percent*100);
 if accuracy_percent < 0.9
+    fprintf('True\n');
     print_rc(process.Kf);
+    fprintf('Estimated\n');
     print_rc(squeeze(trace.trace.Kf(end,:,:,:)));
 end
 
@@ -40,10 +57,12 @@ result = isequalntol(squeeze(trace.trace.Kb(end,:,:,:)),...
     process.Kb, 'AbsTol', 0.3);
 idx = abs(process.Kb) > 0;
 accuracy_percent = sum(result(idx))/length(result(idx));
-fprintf('Kb is %d%% OK\n',accuracy_percent*100);
+fprintf('Kb is %0.0f%% OK\n',accuracy_percent*100);
 
 if accuracy_percent < 0.9
+    fprintf('True\n');
     print_rc(process.Kb);
+    fprintf('Estimated\n');
     print_rc(squeeze(trace.trace.Kb(end,:,:,:)));
 end
 end
