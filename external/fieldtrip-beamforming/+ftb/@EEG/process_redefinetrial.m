ebe14f9448e28d0c95463a38b54028d2b56518f0
pchrapka <pchrapka@gmail.com>
2017-01-13 11:32:25 -0500

    updated ftb

--- external/fieldtrip-beamforming/+ftb/@EEG/process_redefinetrial.m
+++ external/fieldtrip-beamforming/+ftb/@EEG/process_redefinetrial.m
@@ -3,12 +3,12 @@ function obj = process_redefinetrial(obj)
 % ft_definetrial
 if obj.check_file(obj.definetrial)
     % define the trial
-    cfg = ft_definetrial(obj.config.ft_definetrial);
-    data = cfg;
+    data_definetrial = ft_definetrial(obj.config.ft_definetrial);
+    data = data_definetrial;
     save(obj.definetrial, 'data');
     
-    datain = ftb.util.loadvar(obj.preprocessed);
-    data = ft_redefinetrial(cfg, datain);
+    data_preprocessed = ftb.util.loadvar(obj.preprocessed);
+    data = ft_redefinetrial(data_definetrial, data_preprocessed);
     save(obj.preprocessed, 'data','-v7.3');
 else
     fprintf('%s: skipping ft_definetrial, already exists\n',...
--- external/fieldtrip-beamforming/+ftb/@EEG/process_timelock.m
+++ external/fieldtrip-beamforming/+ftb/@EEG/process_timelock.m
@@ -2,17 +2,18 @@ function obj = process_timelock(obj)
 
 % ft_timelockanalysis
 if obj.check_file(obj.timelock)
-    cfgin = obj.config.ft_timelockanalysis;
-    if obj.check_file(obj.rejectartifact)
-        cfgin.inputfile = obj.rejectartifact;
-    else
-        cfgin.inputfile = obj.preprocessed;
-    end
-    cfgin.outputfile = obj.timelock;
+    % set up cfg
+    cfg = obj.config.ft_timelockanalysis;
     
-    ft_timelockanalysis(cfgin);
-    % FIXME missing save?
-    warning('i think i''m missing something here');
+    % check if we should data after artifact rejection
+    if obj.check_file(obj.rejectartifact)
+        cfg.inputfile = obj.rejectartifact;
+    else
+        cfg.inputfile = obj.preprocessed;
+    end
+    cfg.outputfile = obj.timelock;
+    
+    ft_timelockanalysis(cfg);
 else
     fprintf('%s: skipping ft_timelockanalysis, already exists\n',...
         strrep(class(obj),'ftb.',''));
