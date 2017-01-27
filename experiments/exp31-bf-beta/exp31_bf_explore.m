f14d1f6ebe49fb41971d63ad49f172cf312df681
Phil Chrapka <pchrapka@gmail.com>
2017-01-27 14:14:55 -0500

    fixed up bugs in explore script

--- experiments/exp31-bf-beta/exp31_bf_explore.m
+++ experiments/exp31-bf-beta/exp31_bf_explore.m
@@ -3,8 +3,8 @@
 stimulus = 'std';
 subject = 6;
 deviant_percent = 10;
-% patches_type = 'aal';
-patches_type = 'aal-coarse-13';
+patches_type = 'aal';
+% patches_type = 'aal-coarse-13';
 
 [~,data_name,~] = get_data_andrew(subject,deviant_percent);
 
@@ -14,17 +14,28 @@ pipeline.process();
 
 %%
 
-eeg = loadfile('output/std-s06-10/ft_rejectartifact.mat');
+data_name2 = sprintf('%s-%s',stimulus,data_name);
+eeg = loadfile(fullfile('output',data_name2,'ft_rejectartifact.mat');
+
+%%
+cfg = [];
+cfg.viewmode = 'vertical';
+ft_databrowser(cfg,eeg);
+
+%%
 sources = loadfile(pipeline.steps{end}.sourceanalysis);
 lf = loadfile(pipeline.steps{end}.lf.leadfield);
 
-% convert source analysis to EEG data structure
+%% convert source analysis to EEG data structure
+ntrials = length(sources.trial);
 data = copyfields(eeg,[],{'fsample','trialinfo','sampleinfo'});
 data.label = lf.filter_label(lf.inside);
 for i=1:ntrials
     data.trial{i} = cell2mat(sources.trial(i).mom(sources.inside));
-    data.time{i} = source.time;
+    data.time{i} = sources.time;
 end
 
 %%
-ft_databrowser([],data);
+cfg = [];
+cfg.viewmode = 'vertical';
+ft_databrowser(cfg,data);
