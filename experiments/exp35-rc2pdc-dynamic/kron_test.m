c8efd5473192a86c3f8c7514a547b7eb915baa05
pchrapka <pchrapka@gmail.com>
2017-01-28 10:23:55 -0500

    added a few more versions

---
+++ experiments/exp35-rc2pdc-dynamic/kron_test.m
@@ -0,0 +1,11 @@
+function K = kron_test(A,n)
+% computes kron(A,eye(n))
+
+C = cell(n,1);
+[C{:}] = deal(A);
+K = blkdiag(C{:});
+% Rearrange rows and columns
+I = reshape(reshape(1:(size(A,1)*n),size(A,1),n)',size(A,1)*n,1);
+J = reshape(reshape(1:(size(A,2)*n),size(A,2),n)',size(A,2)*n,1);
+K = K(I,J);
+end
