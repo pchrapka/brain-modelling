2cffdbd5dd2936d90f38e631df95d5e8016b9adc
pchrapka <pchrapka@gmail.com>
2017-02-01 13:23:26 -0500

    added triplet trial selection function

---
+++ methods/util/+fthelpers/ft_trialfun_triplet.m
@@ -0,0 +1,95 @@
+function [trl,event] = ft_trialfun_triplet(cfg)
+% selects events in between two specified events
+%
+%   fields required in config
+%   cfg.trialmid.eventtype
+%   cfg.trialmid.eventvalue
+%   cfg.trialpre.eventtype
+%   cfg.trialpre.eventvalue
+%   cfg.trialpost.eventtype
+%   cfg.trialpost.eventvalue
+%   cfg.prestim
+%   cfg.poststim
+
+if ~isfield(cfg,'dataset') || isempty(cfg.dataset)
+    error('MATLAB:nonExistentField','missing dataset');
+end
+
+if ~isfield(cfg,'prestim')
+    error('missing prestim');
+end
+if ~isfield(cfg,'poststim')
+    error('missing poststim');
+end
+
+% check eventvalue
+fields = {'trialmid','trialpre','trialpost'};
+for i=1:length(fields)
+    if ~isfield(cfg,fields{i})
+        error('missing field %s',fields{i});
+    end
+    
+    if ~ischar(cfg.(fields{i}).eventtype)
+        error([mfilename ':input'],...
+                '%s definition can only contain one event type',fields{i});
+    end
+    
+    if isnumeric(cfg.(fields{i}).eventvalue)
+        if length(cfg.(fields{i}).eventvalue) ~= 1
+            error([mfilename ':input'],...
+                '%s definition can only contain one event value',fields{i});
+        end
+%    elseif ischar(cfg.(fields{i}).eventvalue)
+%        cfg.(fields{i}).eventvalue = {cfg.(fields{i}).eventvalue};
+    else
+        errorr([mfilename ':input'],...
+            '%s definition can only contain one event value',fields{i});
+    end
+end
+
+%% the first part is common to all trial functions
+% read the header (needed for the samping rate) and the events
+hdr = ft_read_header(cfg.dataset);
+event  = ft_read_event(cfg.dataset);
+
+%% from here on it becomes specific to the experiment and the data format
+% for the events of interest, find the sample numbers (these are integers)
+% for the events of interest, find the trigger values (these are strings in the case of BrainVision)
+% EVsample   = [event.sample]';
+EVvalue    = {event.value}';
+EVtype     = {event.type}';
+
+% select potential pre events of same type
+pre_event_type = find(strcmp(cfg.trialpre.eventtype, EVtype)==1);
+pre_event_value = find(strcmp(cfg.trialpre.eventvalue, EVvalue)==1);
+pre_event = intersect(pre_event_type,pre_event_value); % indices
+
+% select potential mid events
+mid_event_type = find(strcmp(cfg.trialmid.eventtype, EVtype)==1);
+mid_event_value = find(strcmp(cfg.trialmid.eventvalue, EVvalue)==1);
+mid_event = intersect(mid_event_type,mid_event_value); % indices
+
+% select potential post events
+post_event_type = find(strcmp(cfg.trialpost.eventtype, EVtype)==1);
+post_event_value = find(strcmp(cfg.trialpost.eventvalue, EVvalue)==1);
+post_event = intersect(post_event_type,post_event_value); % indices
+
+% select mid events where the pre event matches
+pre_check = intersect(pre_event+1,mid_event);
+% select mid events where the post event matches
+pre_post_check = intersect(pre_check,post_event-1);
+
+% select samples
+EVsample = [event(pre_post_check).sample]';
+
+trloff = round(-cfg.prestim * hdr.Fs);
+trloff = repmat(trloff,length(pre_post_check),1);
+trldur = round((cfg.prestim + cfg.poststim)*hdr.Fs) - 1;
+trlbeg = EVsample + trloff;
+trlend = trlbeg + trldur;
+
+%% the last part is again common to all trial functions
+% return the trl matrix (required)
+trl = [trlbeg trlend trloff];
+
+end
---
+++ methods/util/+fthelpers/tests/Test_ft_trialfun_triplet.m
@@ -0,0 +1,112 @@
+classdef Test_ft_trialfun_triplet < matlab.unittest.TestCase
+    properties
+        config;
+        dataset;
+    end
+    
+    methods (TestClassSetup)
+        function setup(testCase)
+            
+            % subject specific info
+            [data_file,~,~] = get_data_andrew(3,10);
+            
+            testCase.dataset = data_file;
+
+            % error free config
+            testCase.config = [];
+            testCase.config.dataset = testCase.dataset;
+            
+            testCase.config.trialmid.eventtype = 'STATUS';
+            testCase.config.trialmid.eventvalue = {1}; % standard
+            testCase.config.trialpre.eventtype = 'STATUS';
+            testCase.config.trialpre.eventvalue = {2}; % standard
+            testCase.config.trialpost.eventtype = 'STATUS';
+            testCase.config.trialpost.eventvalue = {1}; % deviant
+            
+            testCase.config.prestim = 0.2; % in seconds
+            testCase.config.poststim = 0.5; % in seconds
+
+        end
+    end
+    
+    methods (Test)
+        function test_ft_trialfun_triplet(testCase)
+            cfg = testCase.config;
+            
+            [trl,event] = ft_trialfun_triplet(cfg);
+            
+            testCase.verifyEqual(size(trl),[279 3]);
+            testCase.verifyFalse(isempty(event));
+        end
+        
+        function test_ft_definetrial(testCase)
+            cfg = testCase.config;
+            cfg.trialfun = 'ft_trialfun_triplet';
+            
+            cfgout = ft_definetrial(cfg);
+            
+            testCase.verifyEqual(size(cfgout.trl),[279 3]);
+            testCase.verifyFalse(isempty(cfgout.event));
+        end
+        
+        function test_ft_trialfun_triplet_error_trialpre_eventvalue(testCase)
+            cfg = testCase.config;
+            
+            cfg.trialpre.eventvalue = {'S 16'}; % deviant
+            testCase.verifyError(@() ft_trialfun_triplet(cfg),...
+                'ft_trialfun_triplet:trialpre');
+            
+            cfg.trialpre.eventvalue = {'S 16','S 11'};
+            testCase.verifyError(@() ft_trialfun_triplet(cfg),...
+                'ft_trialfun_triplet:trialpre');
+        end
+        
+        function test_ft_trialfun_triplet_error_trialpost_eventvalue(testCase)
+            cfg = testCase.config;
+            
+            cfg.trialpost.eventvalue = {'S 16'}; % deviant
+            testCase.verifyError(@() ft_trialfun_triplet(cfg),...
+                'ft_trialfun_triplet:trialpost');
+            
+            cfg.trialpost.eventvalue = {'S 16','S 11'};
+            testCase.verifyError(@() ft_trialfun_triplet(cfg),...
+                'ft_trialfun_triplet:trialpost');
+        end
+        
+        function test_ft_trialfun_triplet_error_dataset(testCase)
+            cfg = testCase.config;
+            cfg.dataset = '';
+            
+            testCase.verifyError(@() ft_trialfun_triplet(cfg),...
+                'MATLAB:nonExistentField');
+        end
+        
+        function test_ft_trialfun_triplet_error_trialpost(testCase)
+            cfg = testCase.config;
+            cfg = rmfield(cfg,'trialpost');
+            testCase.verifyError(@() ft_trialfun_triplet(cfg),...
+                'MATLAB:nonExistentField');
+            
+            cfg = testCase.config;
+            cfg.trialpost = [];
+            testCase.verifyError(@() ft_trialfun_triplet(cfg),...
+                'MATLAB:nonStrucReference');
+        end
+        
+        function test_ft_trialfun_triplet_error_trialdef(testCase)
+            cfg = testCase.config;
+            cfg = rmfield(cfg,'trialdef');
+            testCase.verifyError(@() ft_trialfun_triplet(cfg),...
+                'MATLAB:nonExistentField');
+            
+            cfg = testCase.config;
+            cfg.trialdef = [];
+            testCase.verifyError(@() ft_trialfun_triplet(cfg),...
+                'MATLAB:nonStrucReference');
+        end
+        
+%         function test_ft_definetrial(testCase)
+%             % TODO
+%         end
+    end
+end
