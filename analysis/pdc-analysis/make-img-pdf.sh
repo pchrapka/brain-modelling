fab46c465206a9408b13344270b40fea0f5f27b2
pchrapka <pchrapka@gmail.com>
2017-02-18 17:43:45 -0500

    updated freq range

--- analysis/pdc-analysis/make-img-pdf.sh
+++ analysis/pdc-analysis/make-img-pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATASET="2017-02-18-MCMTLOCCD_TWL2-T40-C113-P10-lambda=0.99-gamma=1.00-pdc-dynamic-seed-*-0.0000-0.0488"
+DATASET="2017-02-18-MCMTLOCCD_TWL2-T40-C113-P10-lambda=0.99-gamma=1.00-pdc-dynamic-seed-*-0.0073-0.0122"
 echo "Converting eps to pdf"
 find ./output -name "$DATASET.eps" -exec epstopdf {} \;
 
