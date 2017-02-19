9abb4fb2cc3fbd178f5d2199af1d19b4a0f1f356
pchrapka <pchrapka@gmail.com>
2017-02-19 09:53:51 -0500

    updated setup_parfor with options

--- methods/util/setup_parfor.m
+++ methods/util/setup_parfor.m
@@ -1,28 +1,78 @@
-function setup_parfor()
+function setup_parfor(varargin)
 %SETUP_PARFOR sets up parallel pool for a given computer configuration
-%   SETUP_PARFOR sets up parallel pool for a given computer configuration
+%   SETUP_PARFOR sets up parallel pool for a given computer configuration.
+%   it only sets up a new parallel pool if there isn't one set up. you can
+%   force it by using the flag.
 %
 %   Feel free to add a new configuration
+%
+%   Parameters
+%   ----------
+%   cores (integer)
+%       number of cores to setup, if cores == 0 it defaults to the max for
+%       that particular machine
+%
+%       blade = 12 for R2012a
+%       blade = 20 for R2013b (untested)
+%
+%   force (logical, default = false)
+%       flag to force opening a parallel pool with the specified number of
+%       cores
 
+p = inputParser();
+addParameter(p,'cores',0,@isnumeric);
+addParameter(p,'force',false,@islogical);
+parse(p,varargin{:});
+
+cores = p.Results.cores;
 comp_name = get_compname();
-    
 
 switch comp_name
     case sprintf('blade16.ece.mcmaster.ca\n')
-        if verLessThan('matlab', '8.2.0.29') % R2013b
-            if matlabpool('size') == 0 
-                matlabpool('open', 12);
+        
+        if exist('parpool','file')
+            if cores == 0
+                % set default
+                cores = 20;
             end
         else
-            % close the current pool
-            if ~isempty(gcp)
-                delete(gcp);
+            if cores == 0
+                % set default
+                cores = 12;
             end
-            % set up a new one
-            parpool('local', 20);
         end
+        
     otherwise
-        fprintf('%s: using default config\n', mfilename);
+        if cores == 0
+            cores = feature('numCores');
+        end
+        fprintf('%s: using default config with %d cores\n', mfilename,cores);
+end
+
+if exist('parpool','file')    
+    if isempty(gcp)
+        % set up a new one
+        parpool('local', cores);
+    elseif p.Results.force
+        % force a new one
+        
+        % close the current pool
+        if ~isempty(gcp)
+            delete(gcp);
+        end
+        
+        % set up a new one
+        parpool('local', cores);
+    end
+else
+    if matlabpool('size') == 0
+        % set up a new one
+        matlabpool('open', cores);
+    elseif p.Results.force
+        % force a new one
+        matlabpool('close');
+        matlabpool('open', cores);
+    end
 end
 
 end
