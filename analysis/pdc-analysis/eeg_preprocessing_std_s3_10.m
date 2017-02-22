b7265a9fe7adf27520b0959e8e3909c70e8f8e5a
pchrapka <pchrapka@gmail.com>
2017-02-22 09:45:13 -0500

    moved all eeg processing code to one function

---
+++ analysis/pdc-analysis/eeg_preprocessing_std_s3_10.m
@@ -0,0 +1,35 @@
+function pipeline = eeg_preprocessing_std_s3_10()
+
+stimulus = 'std';
+subject = 3; 
+deviant_percent = 10;
+patches_type = 'aal';
+% patches_type = 'aal-coarse-13';
+
+%% output dir
+
+[data_file,data_name,~] = get_data_andrew(subject,deviant_percent);
+
+% dataset = data_file;
+data_name2 = sprintf('%s-%s',stimulus,data_name);
+analysis_dir = fullfile(get_project_dir(),'analysis','pdc-analysis');
+outdir = fullfile(analysis_dir,'output',data_name2);
+
+%% preprocess data for beamforming
+eeg_preprocessing_andrew(subject,deviant_percent,stimulus,...
+    'patches',patches_type,...
+    'outdir',outdir);
+
+%% beamform sources
+pipeline = build_pipeline_beamformer(paramsbf_sd_andrew(...
+    subject,deviant_percent,stimulus,'patches',patches_type)); 
+pipeline.process();
+
+%% compute induced sources
+eeg_file = fullfile(outdir,'ft_rejectartifact.mat');
+lf_file = pipeline.steps{end}.lf.leadfield;
+sources_file = pipeline.steps{end}.sourceanalysis;
+
+eeg_induced(sources_file, eeg_file, lf_file, 'outdir',outdir);
+
+end
--- analysis/pdc-analysis/tune_model_order.m
+++ analysis/pdc-analysis/tune_model_order.m
@@ -1,37 +1,8 @@
 %% tune_model_order
 
-stimulus = 'std';
-subject = 3; 
-deviant_percent = 10;
-atlas_name = 'aal';
-patches_type = 'aal';
-% patches_type = 'aal-coarse-13';
-
-%% output dir
-
-[data_file,data_name,~] = get_data_andrew(subject,deviant_percent);
-
-% dataset = data_file;
-data_name2 = sprintf('%s-%s',stimulus,data_name);
-analysis_dir = fullfile(get_project_dir(),'analysis','pdc-analysis');
-outdir = fullfile(analysis_dir,'output',data_name2);
-
-%% preprocess data for beamforming
-eeg_preprocessing_andrew(subject,deviant_percent,stimulus,...
-    'patches',patches_type,...
-    'outdir',outdir);
-
-%% beamform sources
-pipeline = build_pipeline_beamformer(paramsbf_sd_andrew(...
-    subject,deviant_percent,stimulus,'patches',patches_type)); 
-pipeline.process();
-
-%% compute induced sources
-eeg_file = fullfile(outdir,'ft_rejectartifact.mat');
+pipeline = eeg_preprocessing_std_s3_10();
 lf_file = pipeline.steps{end}.lf.leadfield;
 sources_file = pipeline.steps{end}.sourceanalysis;
-
-eeg_induced(sources_file, eeg_file, lf_file, 'outdir',outdir);
 
 %% set lattice options
 lf = loadfile(lf_file);
@@ -39,7 +10,6 @@ patch_labels = lf.filter_label(lf.inside);
 patch_labels = cellfun(@(x) strrep(x,'_',' '),...
     patch_labels,'UniformOutput',false);
 npatch_labels = length(patch_labels);
-% patch_centroids = lf.patch_centroid(lf.inside,:);
 clear lf;
 
 nchannels = npatch_labels;
