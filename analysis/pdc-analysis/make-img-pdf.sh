5eac8f90b223372b115d60f7045a7c408740a1da
pchrapka <pchrapka@gmail.com>
2017-02-28 10:02:23 -0500

    added data specific name to pdf

--- analysis/pdc-analysis/make-img-pdf.sh
+++ analysis/pdc-analysis/make-img-pdf.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 DATASET="2017-02-18-MCMTLOCCD_TWL2-T40-C113-P10-lambda=0.99-gamma=1.00-pdc-dynamic-seed-*-0.0073-0.0122"
+DATASETNAME="2017-02-18-MCMTLOCCD_TWL2-T40-C113-P10-lambda=0.99-gamma=1.00-pdc-dynamic-seed-0.0073-0.0122"
 echo "Converting eps to pdf"
 find ./output -name "$DATASET.eps" -exec epstopdf {} \;
 
 echo "Merging pdf"
-find ./output -name "$DATASET.pdf" -print0 | sort -zn | xargs -0 sh -c 'set -x; pdftk "$@" cat output pdc-seed.pdf' "$0"
+find ./output -name "$DATASET.pdf" -print0 | sort -zn | xargs -0 sh -c 'set -x; pdftk "$@" cat output $DATASETNAME.pdf' "$0"
