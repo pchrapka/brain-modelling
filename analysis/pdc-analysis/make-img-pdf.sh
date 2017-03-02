cc8d07bb1da0da5766814a0ac400a993ec4ccdbd
pchrapka <pchrapka@gmail.com>
2017-03-02 18:49:48 -0500

    updated data sets

--- analysis/pdc-analysis/make-img-pdf.sh
+++ analysis/pdc-analysis/make-img-pdf.sh
@@ -2,12 +2,12 @@
 
 DATASETS=(
     #"2017-02-18-MCMTLOCCD_TWL2-T40-C113-P10-lambda=0.99-gamma=1.00-pdc-dynamic-euc-seed-*-0.0073-0.0122"
-    "2017-02-28-MCMTLOCCD_TWL2-T20-C113-P3-lambda=0.9900-gamma=1.000e-04-pdc-dynamic-euc-seed-*-0.0073-0.0122"
-    "2017-02-28-MCMTLOCCD_TWL2-T20-C113-P3-lambda=0.9900-gamma=1.000e-04-pdc-dynamic-diag-seed-*-0.0073-0.0122"
-    "2017-02-28-MCMTLOCCD_TWL2-T20-C113-P3-lambda=0.9900-gamma=1.000e-04-pdc-dynamic-info-seed-*-0.0073-0.0122"
-    "2017-02-28-MCMTLOCCD_TWL2-T20-C13-P3-lambda=0.9900-gamma=1.000e-01-pdc-dynamic-euc-seed-*-0.0073-0.0122"
-    "2017-02-28-MCMTLOCCD_TWL2-T20-C13-P3-lambda=0.9900-gamma=1.000e-01-pdc-dynamic-diag-seed-*-0.0073-0.0122"
-    "2017-02-28-MCMTLOCCD_TWL2-T20-C13-P3-lambda=0.9900-gamma=1.000e-01-pdc-dynamic-info-seed-*-0.0073-0.0122")
+    #"2017-02-28-MCMTLOCCD_TWL2-T20-C113-P3-lambda=0.9900-gamma=1.000e-04-pdc-dynamic-euc-seed-*-0.0073-0.0122"
+    #"2017-02-28-MCMTLOCCD_TWL2-T20-C113-P3-lambda=0.9900-gamma=1.000e-04-pdc-dynamic-diag-seed-*-0.0073-0.0122"
+    #"2017-02-28-MCMTLOCCD_TWL2-T20-C113-P3-lambda=0.9900-gamma=1.000e-04-pdc-dynamic-info-seed-*-0.0073-0.0122"
+    "2017-03-02-MCMTLOCCD_TWL2-T20-C19-P3-lambda=0.9900-gamma=1.000e-01-pdc-dynamic-euc-seed-*-0.0073-0.0122"
+    "2017-03-02-MCMTLOCCD_TWL2-T20-C19-P3-lambda=0.9900-gamma=1.000e-01-pdc-dynamic-diag-seed-*-0.0073-0.0122"
+    "2017-03-02-MCMTLOCCD_TWL2-T20-C19-P3-lambda=0.9900-gamma=1.000e-01-pdc-dynamic-info-seed-*-0.0073-0.0122")
 
 for DATASET in "${DATASETS[@]}"
 do
