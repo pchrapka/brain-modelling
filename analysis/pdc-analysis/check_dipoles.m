8038c32931070e24510471a2304c28de27cdcced
Phil Chrapka <pchrapka@gmail.com>
2017-03-03 09:41:09 -0500

    finished up check dipoles script

--- analysis/pdc-analysis/check_dipoles.m
+++ analysis/pdc-analysis/check_dipoles.m
@@ -9,7 +9,10 @@ patch_type = 'aal-coarse-19';
     stimulus,subject,deviant_percent,patch_type);
 
 %% get original leadfield
-lf_file = pipeline.steps{end}.lf.leadfield;
+lf_file = pipeline.steps{end}.get_dep('ftb.Leadfield').leadfield;
+lf = loadfile(lf_file);
+source_file = pipeline.steps{end}.sourceanalysis;
+sources = loadfile(source_file);
 
 %%
 atlas_file = fullfile(ft_get_dir(),'template','atlas','aal','ROI_MNI_V4.nii');
@@ -20,11 +23,38 @@ atlas = ft_read_atlas(atlas_file);
 % in Talaraich coordinates
 loc1 = [ -45.0, -3.2, 16.2];
 loc2 = [ 45.0, -3.2, 16.2];
+locs = [loc1; loc2];
+locsmni = tal2mni(locs);
 
-cfg = [];
-cfg.inputcoord = 'tal';
-cfg.atlas = atlas;
-cfg.roi = [loc1; loc2];
-cfg.round2nearestvoxel = 'yes';
+nlocs = size(locsmni,1);
 
-ft_volumedownsample(cfg,
+for i=1:nlocs
+    % create mask
+    cfg = [];
+    cfg.inputcoord = 'mni';
+    cfg.atlas = atlas;
+    cfg.roi = locsmni(i,:);
+    cfg.round2nearestvoxel = 'yes';
+    cfg.sphere = 5; % don't know what units
+    
+    mask = ft_volumelookup(cfg,lf);
+    
+    
+    lf2 = lf;
+    lf2.mask = mask;
+    
+    % get mask labels
+    cfg = [];
+    cfg.inputcoord = 'mni';
+    cfg.atlas = atlas;
+    cfg.maskparameter = 'mask';
+    labels = ft_volumelookup(cfg,lf2);
+    
+    [tmp ind] = sort(labels.count,1,'descend');
+    sel = find(tmp);
+    for j = 1:length(sel)
+        found_areas{j,1} = [num2str(labels.count(ind(j))) ': ' labels.name{ind(j)}];
+    end
+    
+    disp(found_areas);
+end
