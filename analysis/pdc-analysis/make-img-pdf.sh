40cc27bf6b2098ba68070b570ed5fd9ed5213a27
pchrapka <pchrapka@gmail.com>
2017-03-06 20:46:15 -0500

    fixed up bash script, couldn't do it in the makefile

--- analysis/pdc-analysis/make-img-pdf.sh
+++ analysis/pdc-analysis/make-img-pdf.sh
@@ -5,20 +5,49 @@ DATASETS=(
     #"2017-02-28-MCMTLOCCD_TWL2-T20-C113-P3-lambda=0.9900-gamma=1.000e-04-pdc-dynamic-euc-seed-*-0.0073-0.0122"
     #"2017-02-28-MCMTLOCCD_TWL2-T20-C113-P3-lambda=0.9900-gamma=1.000e-04-pdc-dynamic-diag-seed-*-0.0073-0.0122"
     #"2017-02-28-MCMTLOCCD_TWL2-T20-C113-P3-lambda=0.9900-gamma=1.000e-04-pdc-dynamic-info-seed-*-0.0073-0.0122"
-    "2017-03-02-MCMTLOCCD_TWL2-T20-C19-P3-lambda=0.9900-gamma=1.000e-01-pdc-dynamic-euc-seed-*-0.0073-0.0122"
-    "2017-03-02-MCMTLOCCD_TWL2-T20-C19-P3-lambda=0.9900-gamma=1.000e-01-pdc-dynamic-diag-seed-*-0.0073-0.0122"
-    "2017-03-02-MCMTLOCCD_TWL2-T20-C19-P3-lambda=0.9900-gamma=1.000e-01-pdc-dynamic-info-seed-*-0.0073-0.0122")
+    
+    # C19 no envelope
+    #"2017-03-02-MCMTLOCCD_TWL2-T20-C19-P3-lambda=0.9900-gamma=1.000e-01-pdc-dynamic-euc-seed-*-0.0073-0.0122"
+    #"2017-03-02-MCMTLOCCD_TWL2-T20-C19-P3-lambda=0.9900-gamma=1.000e-01-pdc-dynamic-diag-seed-*-0.0073-0.0122"
+    #"2017-03-02-MCMTLOCCD_TWL2-T20-C19-P3-lambda=0.9900-gamma=1.000e-01-pdc-dynamic-info-seed-*-0.0073-0.0122"
+    
+    # C19 envelope
+    "2017-03-02-MCMTLOCCD_TWL2-T20-C19-P3-lambda=0.9800-gamma=1.000e-03-pdc-dynamic-euc-seed-*-0.0073-0.0122"
+    "2017-03-02-MCMTLOCCD_TWL2-T20-C19-P3-lambda=0.9800-gamma=1.000e-03-pdc-dynamic-diag-seed-*-0.0073-0.0122"
+    "2017-03-02-MCMTLOCCD_TWL2-T20-C19-P3-lambda=0.9800-gamma=1.000e-03-pdc-dynamic-info-seed-*-0.0073-0.0122"
+
+    # C21 envelope
+    "2017-03-06-MCMTLOCCD_TWL2-T20-C21-P3-lambda=0.9800-gamma=1.000e-03-pdc-dynamic-euc-seed-*-0.0000-0.0049"
+    "2017-03-06-MCMTLOCCD_TWL2-T20-C21-P3-lambda=0.9800-gamma=1.000e-03-pdc-dynamic-diag-seed-*-0.0000-0.0049"
+    "2017-03-06-MCMTLOCCD_TWL2-T20-C21-P3-lambda=0.9800-gamma=1.000e-03-pdc-dynamic-info-seed-*-0.0000-0.0049"
+)
 
 for DATASET in "${DATASETS[@]}"
 do
     DATASETNAME="${DATASET/"-*"/}"
-    echo "Working on $DATASETNAME"
+    echo "Working on "
+    echo "    $DATASETNAME"
 
-    echo "Converting eps to pdf"
-    find ./output -name "$DATASET.eps" -exec epstopdf {} \;
+    OUTFILE=''
 
-    echo "Merging pdf"
+    while read -r EPSFILE; do
+	OUTFILE=${EPSFILE/.eps/.pdf}
+	if [ ! -f $OUTFILE ] || [ $EPSFILE -nt $OUTFILE ]; then
+	    echo "Converting: $EPSFILE"
+	    epstopdf "$EPSFILE"
+	fi
+    done < <(find ./output -name "$DATASET.eps")
+
+    #echo outfile: $OUTFILE
+    IMGDIR=$(dirname "${OUTFILE}")
+    #echo $IMGDIR
+    PDFDIR="${IMGDIR/img/}"
+    #echo $PDFDIR
+
+    echo "Merging"
+    echo "    $DATASETNAME.pdf"
+    find ./output -name "$DATASET.pdf" -print0 | sort -zn | xargs -0 sh -c 'pdftk "$@" cat output '$PDFDIR$DATASETNAME'.pdf' "$0"
+    
     # for debug
-    find ./output -name "$DATASET.pdf" -print0 | sort -zn | xargs -0 sh -c 'set -x; pdftk "$@" cat output '$DATASETNAME'.pdf' "$0"
-    #find ./output -name "$DATASET.pdf" -print0 | sort -zn | xargs -0 sh -c 'pdftk "$@" cat output "$DATASETNAME".pdf' "$0"
+    #find ./output -name "$DATASET.pdf" -print0 | sort -zn | xargs -0 sh -c 'set -x; pdftk "$@" cat output '$DATASETNAME'.pdf' "$0"
 done
