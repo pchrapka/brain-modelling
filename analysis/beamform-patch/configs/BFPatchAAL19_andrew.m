b491489e107b1c771d73a3c66d989757d2cda3ab
pchrapka <pchrapka@gmail.com>
2017-03-10 09:42:46 -0500

    merged BFPatch config funcs into one

--- analysis/beamform-patch/configs/BFPatchAAL19_andrew.m
+++ analysis/beamform-patch/configs/BFPatchAAL19_andrew.m
@@ -1,10 +1,70 @@
-function cfg = BFPatchAAL19_andrew(data_name)
+function cfg = BFPatchAAL19_andrew(data_name,varargin)
 % BFPatchAAL
 
-cfg = [];
-cfg.PatchModel = {'aal-coarse-19'};
+p = inputParser();
+addRequired(p,'data_name',@ischar);
+addParameter(p,'flag_add_auditory',false,@islogical);
+addParameter(p,'flag_outer',false,@islogical);
+addParameter(p,'flag_nocerebellum',false,@islogical);
+parse(p,data_name,varargin{:});
 
-% cfg.get_basis = {};
+if p.Results.flag_outer && p.Results.flag_nocerebellum
+    patchmodel_params = {'type','outer-nocerebellum'};
+    patchmodel_name = 'aal-coarse-19-outer-nocer';
+elseif p.Results.flag_outer
+    patchmodel_params = {'type','outer'};
+    patchmodel_name = 'aal-coarse-19-outer';
+elseif p.Results.flag_nocerebellum
+    error('cannot specify no cerebellum on its own');
+else
+    patchmodel_params = {'type','all'};
+    patchmodel_name = 'aal-coarse-19';
+end
+
+sphere_patch = {};
+if p.Results.flag_add_auditory
+    % P1 dipoles in Talaraich coordinates
+    switch data_name(1:3)
+        case 's03'
+            loc_l = [-34.357361   -8.505583   +9.360396];
+            %         '12: no_label_found'
+            %         '5: Insula_L'
+            %         '3: Putamen_L'
+            %         '3: Thalamus_L'
+            %         '2: Rolandic_Oper_L'
+            %         '2: Caudate_L'
+            %         '2: Heschl_L'
+            %         '1: Precentral_L'
+            %         '1: Pallidum_L'
+            %         '1: Temporal_Sup_L'
+            
+            loc_r = [+34.357361   -8.505583   +9.360396];
+            %         '9: no_label_found'
+            %         '6: Insula_R'
+            %         '5: Putamen_R'
+            %         '4: Thalamus_R'
+            %         '3: Rolandic_Oper_R'
+            %         '3: Caudate_R'
+            %         '1: Pallidum_R'
+            %         '1: Heschl_R'
+        otherwise
+            warning([mfilename ':dipoles'],'using default P1 dipoles');
+            loc_l = [ -45.0, -3.2, 16.2];
+            loc_r = [ 45.0, -3.2, 16.2];
+    end
+    locs = [loc_l; loc_r];
+    locsmni = tal2mni(locs);
+    locsmni = locsmni/10; % convert to cm
+    
+    sphere_patch{1} = {'Auditory Left',locsmni(1,:),'radius',2};
+    sphere_patch{2} = {'Auditory Right',locsmni(2,:),'radius',2};
+    
+    patchmodel_name = sprintf('%s-plus2-rad2',patchmodel_name);
+end
+
+cfg = [];
+cfg.PatchModel = {'aal-coarse-19','params',patchmodel_params,'sphere_patch',sphere_patch};
+
 cfg.cov_avg = 'yes';
 cfg.compute_lcmv_patch_filters = {'mode','single','fixedori',true}; % for saving mem
 % cfg.compute_lcmv_patch_filters = {'mode','all','fixedori',true}; % for plotting
@@ -13,15 +73,18 @@ cfg.ft_sourceanalysis.method = 'lcmv';
 cfg.ft_sourceanalysis.lcmv.keepmom = 'yes';
 cfg.ft_sourceanalysis.lcmv.lambda = '1%';
 switch data_name(1:3)
+    case 's03'
+        % all good
     case 's06'
         cfg.ft_sourceanalysis.channel = {'EEG','-D32','-C10'};
     otherwise
         warning('update bad EEG channels for ft_sourceanalysis in %s',mfilename);
 end
 
-cfg.name = sprintf('%s-%s',...
-    cfg.PatchModel{1},...
-    cfg.ft_sourceanalysis.method);
+cfg.name = sprintf('%s-%s-%s',...
+    patchmodel_name,...
+    cfg.ft_sourceanalysis.method,...
+    data_name(1:3));
 
 [srcdir,~,~] = fileparts(mfilename('fullpath'));
 save(fullfile(srcdir, [strrep(mfilename,'_','-') '.mat']),'cfg');
--- analysis/beamform-patch/configs/paramsbf_sd_andrew.m
+++ analysis/beamform-patch/configs/paramsbf_sd_andrew.m
@@ -24,11 +24,17 @@ switch p.Results.patches
     case 'aal-coarse-19'
         params_bf = BFPatchAAL19_andrew(data_name);
     case 'aal-coarse-19-plus2'
-        params_bf = BFPatchAAL19_plus2_rad2_andrew(data_name);
+        params_bf = BFPatchAAL19_andrew(data_name,...
+            'flag_add_auditory',true);
     case 'aal-coarse-19-outer-plus2'
-        params_bf = BFPatchAAL19_outer_plus2_rad2_andrew(data_name);
+        params_bf = BFPatchAAL19_andrew(data_name,...
+            'flag_add_auditory',true,...
+            'flag_outer',true);
     case 'aal-coarse-19-outer-nocer-plus2'
-        params_bf = BFPatchAAL19_outer_nocer_plus2_rad2_andrew(data_name);
+        params_bf = BFPatchAAL19_andrew(data_name,...
+            'flag_add_auditory',true,...
+            'flag_outer',true,...
+            'flag_nocerebellum',true);
     otherwise
         error('add new BeamformerPatch config for %s',p.Results.patches);
 end
