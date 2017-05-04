9e40d323df743a1372f395a536bcbc7a1addeac1
pchrapka <pchrapka@gmail.com>
2017-05-04 18:40:20 -0400

    added a test script

---
+++ analysis/pdc-analysis/pdc_analysis_params_bootstrap_test.m
@@ -0,0 +1,30 @@
+%% pdc_analysis_params_bootstrap_test
+
+%% aal-coarse-19-outer-nocer-plus2, envelope, eachchannel
+
+% g 1e-6, l 0.99, order 13
+
+params(k).patch_type = 'aal-coarse-19-outer-nocer-plus2';
+params(k).metrics = {'euc','diag','info'};
+params(k).ntrials = 20;
+params(k).order = 14;
+params(k).lambda = 0.99;
+params(k).gamma = 1;
+params(k).normalization = 'eachchannel';
+params(k).envelope = true;
+params(k).prepend_data = 'flipdata';
+params(k).nresamples = 2;
+params(k).alpha = 0.05;
+params(k).null_mode = 'estimate_ind_channels';
+k = k+1;
+
+%% run analysis
+flag_run = true;
+flag_tune = false;
+flag_bootstrap = true;
+
+%% run variations
+pdc_analysis_variations(params,...
+    'flag_run',flag_run,...
+    'flag_tune',flag_tune,...
+    'flag_bootstrap',flag_bootstrap);
