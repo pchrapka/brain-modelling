901243caed26200f9bc438e83e890a9b3a13c3d5
Phil <chrapkpk@mcmaster.ca>
2017-05-20 09:46:02 -0400

    modified some params

--- analysis/pdc-analysis/pdc_analysis_params_bootstrap.m
+++ analysis/pdc-analysis/pdc_analysis_params_bootstrap.m
@@ -67,9 +67,24 @@ params(k).patch_type = 'aal-coarse-19-outer-nocer-plus2';
 % params(k).metrics = {'diag','euc','info'};
 params(k).metrics = {'diag'};
 params(k).ntrials = 20;
-params(k).order = 11;
+params(k).order = 10;
+params(k).lambda = 0.94;
+params(k).gamma = 1.72377e-06;
+params(k).normalization = 'eachchannel';
+params(k).envelope = true;
+params(k).prepend_data = 'flipdata';
+params(k).nresamples = 100;
+params(k).alpha = 0.05;
+params(k).null_mode = 'estimate_ind_channels';
+k = k+1;
+
+params(k).patch_type = 'aal-coarse-19-outer-nocer-plus2';
+% params(k).metrics = {'diag','euc','info'};
+params(k).metrics = {'diag'};
+params(k).ntrials = 20;
+params(k).order = 6;
 params(k).lambda = 0.99;
-params(k).gamma = 0.0008695;
+params(k).gamma = 7.147e-5;
 params(k).normalization = 'eachchannel';
 params(k).envelope = true;
 params(k).prepend_data = 'flipdata';
@@ -81,8 +96,8 @@ k = k+1;
 %% run analysis
 flag_run = true;
 flag_tune = false;
-% flag_bootstrap = true;
-flag_bootstrap = false;
+flag_bootstrap = true;
+% flag_bootstrap = false;
 
 %% run variations
 pdc_analysis_variations(params,...
