9201340958703c7b1c013488ccc24804060982ed
pchrapka <pchrapka@gmail.com>
2017-06-14 08:46:43 -0400

    updated pdf script

--- analysis/pdc-analysis/make-pdf.sh
+++ analysis/pdc-analysis/make-pdf.sh
@@ -1,6 +1,45 @@
 #!/bin/bash
 
-DIR=output/std-s03-10/aal-coarse-19-outer-nocer-plus2/lf-sources-ch12-trials100-samplesall-normeachchannel-envyes-for-filter
-NAME=2017-04-14-MCMTLOCCD_TWL4-T20-C12-P11-lambda=0.9900-gamma=1.000e-05-pdc-dynamic-diag-ds4-seed-*-thresh0.00-0.0000-0.0195
+# # Test
+# DIR=output/std-s03-10/aal-coarse-19-outer-nocer-plus2/lf-sources-ch12-trials100-samplesall-normeachchannel-envyes-for-filter
+# NAME=2017-04-14-MCMTLOCCD_TWL4-T20-C12-P11-lambda=0.9900-gamma=1.000e-05-pdc-dynamic-diag-ds4-seed-*-thresh0.00-0.0000-0.0195
 
-./make-pdf-from-images.sh --dir=$DIR --name=$NAME
+# ./make-pdf-from-images.sh --dir=$DIR --name=$NAME
+
+DATA=(
+    # LEFT 
+    "output/std-s03-10/aal-coarse-19-outer-nocer-hemileft-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
+    "2017-06-13-MCMTLOCCD_TWL4-T20-C7-P5-lambda=0.9900-gamma=1.000e-03-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
+
+    "output/std-s03-10/aal-coarse-19-outer-nocer-hemileft-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
+    "2017-06-13-MCMTLOCCD_TWL4-T20-C7-P7-lambda=0.9900-gamma=1.000e-04-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
+    
+    "output/std-s03-10/aal-coarse-19-outer-nocer-hemileft-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
+    "2017-06-13-MCMTLOCCD_TWL4-T20-C7-P5-lambda=0.9900-gamma=1.000e-05-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
+
+    # RIGHT
+    "output/std-s03-10/aal-coarse-19-outer-nocer-hemiright-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
+    "2017-06-13-MCMTLOCCD_TWL4-T20-C7-P6-lambda=0.9900-gamma=1.000e-03-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
+    
+    "output/std-s03-10/aal-coarse-19-outer-nocer-hemiright-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
+    "2017-06-13-MCMTLOCCD_TWL4-T20-C7-P5-lambda=0.9900-gamma=1.000e-04-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
+    
+    "output/std-s03-10/aal-coarse-19-outer-nocer-hemiright-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
+    "2017-06-13-MCMTLOCCD_TWL4-T20-C7-P3-lambda=0.9900-gamma=1.000e-05-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
+    )
+
+index=0
+len=${#DATA[@]}
+while [ $index -lt "$len" ]
+do
+    # get DIR and NAME
+    DIR=${DATA[$index]}
+    NAME=${DATA[$index+1]}
+    echo "DIR: " $DIR
+    echo "NAME: " $NAME
+
+    ./make-pdf-from-images.sh --dir=$DIR --name=$NAME
+
+    # Next pair
+    index=$(($index+2))
+done
