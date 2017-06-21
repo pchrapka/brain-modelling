7ac379135c5dcd03ad33f34ae9dbad2a0113e36f
pchrapka <pchrapka@gmail.com>
2017-06-21 08:04:57 -0400

    updated images

--- analysis/pdc-analysis/make-pdf.sh
+++ analysis/pdc-analysis/make-pdf.sh
@@ -2,37 +2,37 @@
 
 # # Test
 # DIR=output/std-s03-10/aal-coarse-19-outer-nocer-plus2/lf-sources-ch12-trials100-samplesall-normeachchannel-envyes-for-filter
-# NAME=2017-04-14-MCMTLOCCD_TWL4-T20-C12-P11-lambda=0.9900-gamma=1.000e-05-pdc-dynamic-diag-ds4-seed-*-thresh0.00-0.0000-0.0195
+# NAME=2017-04-14-MCMTLOCCD_TWL4-T20-C12-P11-lambda0.9900-gamma1.000e-05-pdc-dynamic-diag-ds4-seed-*-thresh0.00-0.0000-0.0195
 
 # ./make-pdf-from-images.sh --dir=$DIR --name=$NAME
 
 DATA=(
     # LEFT 
     "output/std-s03-10/aal-coarse-19-outer-nocer-hemileft-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
-    "2017-06-13-MCMTLOCCD_TWL4-T20-C7-P5-lambda=0.9900-gamma=1.000e-03-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
+    "2017-06-20-MCMTLOCCD_TWL4-T20-C7-P5-lambda0.9900-gamma1.000e-03-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
 
     "output/std-s03-10/aal-coarse-19-outer-nocer-hemileft-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
-    "2017-06-13-MCMTLOCCD_TWL4-T20-C7-P7-lambda=0.9900-gamma=1.000e-04-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
+    "2017-06-20-MCMTLOCCD_TWL4-T20-C7-P7-lambda0.9900-gamma1.000e-04-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
 
     # a few tries for gamma 1e-4
-    "output/std-s03-10/aal-coarse-19-outer-nocer-hemileft-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
-    "2017-06-14-MCMTLOCCD_TWL4-T20-C7-P5-lambda=0.9900-gamma=1.000e-04-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
+    #"output/std-s03-10/aal-coarse-19-outer-nocer-hemileft-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
+    #"2017-06-14-MCMTLOCCD_TWL4-T20-C7-P5-lambda0.9900-gamma1.000e-04-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
 
-    "output/std-s03-10/aal-coarse-19-outer-nocer-hemileft-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
-    "2017-06-14-MCMTLOCCD_TWL4-T20-C7-P14-lambda=0.9900-gamma=1.000e-04-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
+    #"output/std-s03-10/aal-coarse-19-outer-nocer-hemileft-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
+    #"2017-06-14-MCMTLOCCD_TWL4-T20-C7-P14-lambda0.9900-gamma1.000e-04-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
     
     "output/std-s03-10/aal-coarse-19-outer-nocer-hemileft-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
-    "2017-06-13-MCMTLOCCD_TWL4-T20-C7-P5-lambda=0.9900-gamma=1.000e-05-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
+    "2017-06-20-MCMTLOCCD_TWL4-T20-C7-P5-lambda0.9900-gamma1.000e-05-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
 
     # RIGHT
     "output/std-s03-10/aal-coarse-19-outer-nocer-hemiright-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
-    "2017-06-13-MCMTLOCCD_TWL4-T20-C7-P6-lambda=0.9900-gamma=1.000e-03-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
+    "2017-06-20-MCMTLOCCD_TWL4-T20-C7-P6-lambda0.9900-gamma1.000e-03-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
 
     "output/std-s03-10/aal-coarse-19-outer-nocer-hemiright-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
-    "2017-06-14-MCMTLOCCD_TWL4-T20-C7-P7-lambda=0.9900-gamma=1.000e-04-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
+    "2017-06-20-MCMTLOCCD_TWL4-T20-C7-P7-lambda0.9900-gamma1.000e-04-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
     
     "output/std-s03-10/aal-coarse-19-outer-nocer-hemiright-audr2-v1r2/lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata"
-    "2017-06-13-MCMTLOCCD_TWL4-T20-C7-P3-lambda=0.9900-gamma=1.000e-05-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
+    "2017-06-20-MCMTLOCCD_TWL4-T20-C7-P3-lambda0.9900-gamma1.000e-05-p1-removed-pdc-dynamic-diag-f512-ds4-seed-*-opnone-thresh0.00-0.0000-0.0098"
     )
 
 index=0
--- analysis/pdc-analysis/paper_copy_images.m
+++ analysis/pdc-analysis/paper_copy_images.m
@@ -10,25 +10,25 @@ k = 1;
 
 images(k).file = fullfile('output','std-s03-10','aal-coarse-19-outer-nocer-hemileft-audr2-v1r2',...
     'lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata','img',...
-    '2017-06-13-MCMTLOCCD_TWL4-T20-C7-P5-lambda0.9900-gamma1.000e-05-p1-removed-pdc-dynamic-diag-f512-ds4-seed-in-i5-opnone-thresh0.00-0.0000-0.0098.eps');
+    '2017-06-20-MCMTLOCCD_TWL4-T20-C7-P5-lambda0.9900-gamma1.000e-05-p1-removed-pdc-dynamic-diag-f512-ds4-seed-in-i5-opnone-thresh0.00-0.0000-0.0098.eps');
 images(k).file_out = 'hemileft-to-auditory-left-paper.eps';
 k = k+1;
 
 images(k).file = fullfile('output','std-s03-10','aal-coarse-19-outer-nocer-hemileft-audr2-v1r2',...
     'lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata','img',...
-    '2017-06-13-MCMTLOCCD_TWL4-T20-C7-P5-lambda0.9900-gamma1.000e-05-p1-removed-pdc-dynamic-diag-f512-ds4-seed-in-i1-opnone-thresh0.00-0.0000-0.0098.eps');
+    '2017-06-20-MCMTLOCCD_TWL4-T20-C7-P5-lambda0.9900-gamma1.000e-05-p1-removed-pdc-dynamic-diag-f512-ds4-seed-in-i1-opnone-thresh0.00-0.0000-0.0098.eps');
 images(k).file_out = 'hemileft-to-temporal-left-paper.eps';
 k = k+1;
 
 images(k).file = fullfile('output','std-s03-10','aal-coarse-19-outer-nocer-hemileft-audr2-v1r2',...
     'lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata','img',...
-    '2017-06-13-MCMTLOCCD_TWL4-T20-C7-P5-lambda0.9900-gamma1.000e-05-p1-removed-pdc-dynamic-diag-f512-ds4-adjacency-idx96-384-0.0000-0.0098.eps');
+    '2017-06-20-MCMTLOCCD_TWL4-T20-C7-P5-lambda0.9900-gamma1.000e-05-p1-removed-pdc-dynamic-diag-f512-ds4-adjacency-idx192-768-0.0000-0.0098.eps');
 images(k).file_out = 'hemileft-adjacency-summary.eps';
 k = k+1;
 
 images(k).file = fullfile('output','std-s03-10','aal-coarse-19-outer-nocer-hemiright-audr2-v1r2',...
     'lf-data-trialsall-samplesall-normeachchannel-envyes-prependflipdata','img',...
-    '2017-06-13-MCMTLOCCD_TWL4-T20-C7-P3-lambda0.9900-gamma1.000e-05-p1-removed-pdc-dynamic-diag-f512-ds4-adjacency-idx96-384-0.0000-0.0098.eps');
+    '2017-06-20-MCMTLOCCD_TWL4-T20-C7-P3-lambda0.9900-gamma1.000e-05-p1-removed-pdc-dynamic-diag-f512-ds4-adjacency-idx192-768-0.0000-0.0098.eps');
 images(k).file_out = 'hemiright-adjacency-summary.eps';
 k = k+1;
 
@@ -38,5 +38,7 @@ for i=1:length(images)
     [success,message,messageid] = copyfile(images(i).file,outfile);
     if success ~= 1
         warning('could not copy %s',images(i).file);
+        disp(message);
+        disp(messageid);
     end
 end
