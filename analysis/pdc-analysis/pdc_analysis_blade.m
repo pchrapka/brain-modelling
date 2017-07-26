51415d0990c1495e36f81e502b2b25fd9bc05faf
pchrapka <pchrapka@gmail.com>
2017-07-26 09:06:55 -0400

    added both hemis to s5 10 trials

--- analysis/pdc-analysis/pdc_analysis_blade.m
+++ analysis/pdc-analysis/pdc_analysis_blade.m
@@ -1,11 +1,10 @@
 %% pdc_analysis_blade
 
-% pdc_analysis_s3_std
-% pdc_analysis_s3_std_perms
-pdc_analysis_s3_std_100trials
-
 % pdc_analysis_s5_std
 % pdc_analysis_s5_std_perms
 pdc_analysis_s5_std_100trials
 
-pdc_analysis_s3_std_bootstrap
+% pdc_analysis_s3_std
+pdc_analysis_s3_std_perms
+% pdc_analysis_s3_std_100trials
+% pdc_analysis_s3_std_bootstrap
