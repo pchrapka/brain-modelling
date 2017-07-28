580d632b6e91421d1542cb19b11355488943eb0c
pchrapka <pchrapka@gmail.com>
2017-07-28 08:37:07 -0400

    added permutations of 100 trial set up for s3

--- analysis/pdc-analysis/pdc_analysis_blade.m
+++ analysis/pdc-analysis/pdc_analysis_blade.m
@@ -2,9 +2,10 @@
 
 % pdc_analysis_s5_std
 % pdc_analysis_s5_std_perms
-pdc_analysis_s5_std_100trials
+% pdc_analysis_s5_std_100trials
 
 % pdc_analysis_s3_std
-pdc_analysis_s3_std_perms
+% pdc_analysis_s3_std_perms
 % pdc_analysis_s3_std_100trials
 % pdc_analysis_s3_std_bootstrap
+pdc_analysis_s3_std_100trials_perms
